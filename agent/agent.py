4e53689061bdf516c06214017b03183ae7c2006f
Will Metcalf <william.metcalf@gmail.com>
2020-01-15 15:33:49 +0000

    Fix for overkill

--- agent/agent.py
+++ agent/agent.py
@@ -432,4 +432,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("host", nargs="?", default="0.0.0.0")
     parser.add_argument("port", nargs="?", default="8000")
+    args = parser.parse_args()
     app.run(host=args.host, port=int(args.port))

8cff0f8c97f3c7c9553cfb861392d64b845c78c2
doomedraven <doomedraven@users.noreply.github.com>
2020-01-22 21:44:37 +0100

    Update cape_utils.py

--- lib/cuckoo/common/cape_utils.py
+++ lib/cuckoo/common/cape_utils.py
@@ -36,7 +36,7 @@ except ImportError:
     HAS_MALWARECONFIGS = False
     print("Missed RATDecoders -> pip3 install git+https://github.com/kevthehermit/RATDecoders")
 
-cape_decoders = os.path.join(CUCKOO_ROOT, "modules", "processing", "parsers", "malwareconfig")
+cape_decoders = os.path.join(CUCKOO_ROOT, "modules", "processing", "parsers", "CAPE")
 CAPE_DECODERS = [
     os.path.basename(decoder)[:-3]
     for decoder in glob.glob(cape_decoders + "/[!_]*.py")

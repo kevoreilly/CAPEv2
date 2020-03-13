766f9c500614e17b45d6abd51b68ab1e0191ce50
DoomedRaven <doommedraven@gmail.com>
2020-03-13 14:18:24 +0100

    Update cape_utils.py

--- lib/cuckoo/common/cape_utils.py
+++ lib/cuckoo/common/cape_utils.py
@@ -216,7 +216,8 @@ def static_config_parsers(yara_hit, file_data, cape_config):
                     for (key, value) in malwareconfig_config.items():
                         cape_config["cape_config"].update({key: [value]})
             except Exception as e:
-                log.error("CAPE: malwareconfig parsing error with %s: %s", cape_name, e)
+                log.warning(
+                    "malwareconfig parsing error with %s: %s, you should submit issue/fix to https://github.com/kevthehermit/RATDecoders/", cape_name, e)
 
             if "cape_config" in cape_config:
                 if cape_config["cape_config"] == {}:

22f83a7ccb27cb07e187d314c9d35a193a612cb8
DoomedRaven <doommedraven@gmail.com>
2020-03-19 18:32:59 +0100

    Create test_suricata_naming.py

---
+++ tests/test_suricata_naming.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import os, sys
+
+if sys.version_info[:2] < (3, 5):
+    sys.exit("You are running an incompatible version of Python, please use >= 3.5")
+
+CUCKOO_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
+sys.path.append(CUCKOO_ROOT)
+
+from lib.cuckoo.core.plugins import get_suricata_family
+
+#ET MALWARE Sharik/Smoke CnC Beacon 11
+#ETPRO TROJAN MSIL/Revenge-RAT CnC Checkin
+#ETPRO TROJAN Win32/Predator The Thief Initial CnC Checkin
+
+assert "Sharik/Smoke" == get_suricata_family("ET MALWARE Sharik/Smoke CnC Beacon 11")
+assert "Revenge-RAT" == get_suricata_family("ETPRO TROJAN MSIL/Revenge-RAT CnC Checkin")
+assert "Predator" == get_suricata_family("ETPRO TROJAN Win32/Predator The Thief Initial CnC Checkin")

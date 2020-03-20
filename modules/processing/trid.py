da9950b5718283b3fe8b247e5d1a56cbaea13ebf
DoomedRaven <doommedraven@gmail.com>
2020-03-20 12:49:08 +0100

    add trid fix info

--- modules/processing/trid.py
+++ modules/processing/trid.py
@@ -6,10 +6,12 @@
 from __future__ import absolute_import
 import os.path
 import subprocess
-
+import logging
 from lib.cuckoo.common.abstracts import Processing
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.common.exceptions import CuckooProcessingError
+
+log = logging.getLogger()
 
 class TrID(Processing):
     """Extract TrID output from file."""
@@ -28,8 +30,12 @@ class TrID(Processing):
             trid_binary = os.path.join(CUCKOO_ROOT, self.options.get("identifier", "trid/trid"))
             definitions = os.path.join(CUCKOO_ROOT, self.options.get("definitions", "trid/triddefs.trd"))
 
-        output = subprocess.check_output([ trid_binary, "-d:%s" % definitions, self.file_path], stderr=subprocess.STDOUT)
-        strings = output.split('\n')
-        # trim data
-        strings = strings[6:-1]
+        try:
+            output = subprocess.check_output([ trid_binary, "-d:%s" % definitions, self.file_path], stderr=subprocess.STDOUT, universal_newlines=True)
+            strings = output.split('\n')
+            # trim data
+            strings = strings[6:-1]
+        except subprocess.CalledProcessError:
+            log.warning("You need to configure your server to make TrID work properly")
+            log.warning("sudo rm -f /usr/lib/locale/locale-archive && sudo locale-gen --no-archive")
         return strings

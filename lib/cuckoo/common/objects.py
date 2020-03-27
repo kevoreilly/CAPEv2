b6977563088593e3e03becdc4c5f2f5361b58fb4
DoomedRaven <doommedraven@gmail.com>
2020-03-27 18:06:34 +0100

    Update objects.py

--- lib/cuckoo/common/objects.py
+++ lib/cuckoo/common/objects.py
@@ -131,7 +131,7 @@ def IsPEImage(buf, size=False):
         size = len(buf)
     if size < DOS_HEADER_LIMIT:
         return False
-    buf = buf.encode("utf-8")
+    #buf = buf.encode("utf-8")
     dos_header = buf[:DOS_HEADER_LIMIT]
     nt_headers = None
 

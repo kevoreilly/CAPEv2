1531c5010824d1ce0f8b32a642ecc68d78b4d46f
DoomedRaven <doommedraven@gmail.com>
2020-05-12 20:12:45 +0200

    Update cape_utils.py

--- lib/cuckoo/common/cape_utils.py
+++ lib/cuckoo/common/cape_utils.py
@@ -29,6 +29,7 @@ except ImportError:
 #Import All config parsers
 try:
     import mwcp
+    mwcp.logging.disable(logging.WARNING)
     mwcp.register_parser_directory(os.path.join(CUCKOO_ROOT, "modules", "processing", "parsers", "mwcp"))
     malware_parsers = {block.name.split(".")[-1]:block.name for block in mwcp.get_parser_descriptions(config_only=False)}
     HAS_MWCP = True

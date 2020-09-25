57ef64eb5063fe884f3050a940050e864541b17f
unknown <kevoreilly@gmail.com>
2020-09-25 10:32:20 +0100

    Fix syntax error in Shellcode-Unpacker package

--- analyzer/windows/modules/packages/Shellcode-Unpacker.py
+++ analyzer/windows/modules/packages/Shellcode-Unpacker.py
@@ -11,7 +11,7 @@ from lib.common.abstracts import Package
 
 log = logging.getLogger(__name__)
 
-class Shellcode-Unpacker(Package):
+class Shellcode_Unpacker(Package):
     """32-bit Shellcode Unpacker package."""
 
     def __init__(self, options={}, config=None):

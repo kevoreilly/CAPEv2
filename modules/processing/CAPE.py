8f055939928ac30ddc2f648e77ad1c8bbdcf5225
DoomedRaven <doommedraven@gmail.com>
2020-10-23 19:14:29 +0200

    Update CAPE.py

--- modules/processing/CAPE.py
+++ modules/processing/CAPE.py
@@ -350,7 +350,7 @@ class CAPE(Processing):
                 config[cape_name] = dict()
                 config[cape_name]["cape_type"] = "QakBot Config"
                 config_tmp = static_config_parsers(cape_name, file_data)
-                if config_tmp and config_tmp[hit["name"].replace("_", " ")]:
+                if config_tmp and config_tmp[cape_name]:
                     config.update(config_tmp)
                 append_file = False
             # Attempt to decrypt script dump

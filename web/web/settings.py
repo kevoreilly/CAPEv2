74834be60588acd9ba66e3c40c62176ebf062636
DoomedRaven <doommedraven@gmail.com>
2020-10-31 17:06:46 +0100

    Update settings.py

--- web/web/settings.py
+++ web/web/settings.py
@@ -57,6 +57,8 @@ zip_cfg = aux_cfg.zipped_download
 URL_ANALYSIS = web_cfg.url_analysis.get("enabled", False)
 DLNEXEC = web_cfg.dlnexec.get("enabled", False)
 ZIP_PWD = zip_cfg.get("zip_pwd", b"infected")
+if not isinstance(ZIP_PWD, bytes):
+    ZIP_PWD = ZIP_PWD.encode("utf-8")
 MOLOCH_BASE = moloch_cfg.get("base", None)
 MOLOCH_NODE = moloch_cfg.get("node", None)
 MOLOCH_ENABLED = moloch_cfg.get("enabled", False)

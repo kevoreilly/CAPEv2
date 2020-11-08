3608e6f53a7b8af29a36bc38f721d3666cf79ed6
Matteo Manzoni <manzoni.matteo@mailfence.com>
2020-11-08 18:14:40 +0100

    BoxJS Integration tweaks (#358)
    * Update AnalysisID retrival for current BoxJS Version
    
    Signed-off-by: Matteo Manzoni <manzoni.matteo@mailfence.com>
    
    * Add retry for yet to complete analysis
    
    The endpoint in the case the analysis has just been submitted will
    return '"server_err": 4' without any "code"
    
    Signed-off-by: Matteo Manzoni <manzoni.matteo@mailfence.com>
    
    * Revert safe return on unknown rcode
    
    Signed-off-by: Matteo Manzoni <manzoni.matteo@mailfence.com>

--- modules/processing/boxjs.py
+++ modules/processing/boxjs.py
@@ -39,7 +39,7 @@ class BoxJS(Processing):
             # log.debug("FLAGS %s" % flags)
             files = {"sample": kwargs.get("sample")}
             r = requests.post(url, timeout=self.timeout, data=flags, files=files)
-            return r.text if r.status_code == 200 else {}
+            return r.json()["analysisID"] if r.status_code == 200 else None
         except (requests.ConnectionError, ValueError) as e:
             raise CuckooOperationalError("Unable to POST to the API server: %r" % e.message)
 
@@ -69,17 +69,19 @@ class BoxJS(Processing):
         base_url = "{}/sample/{}".format(self.url, str(analysis_id))
 
         flags = ""
-        results = {}
+
         # Wait for the analysis to be completed.
         done = False
         while not done:
-            time.sleep(1)
+            time.sleep(2)
             result = self.request_json(base_url)
-            code = result["code"]
+            code = result.get("code", None)
             retry = False
 
             # Read the status code, and retry with different flags if necessary
-            if code == 0:  # Success
+            if code is None:
+                continue
+            elif code == 0:  # Success
                 done = True
             elif code == 1:  # Generic error
                 # We don't know how to handle this, so continue
@@ -100,7 +102,7 @@ class BoxJS(Processing):
             else:
                 done = True
                 log.info("BOXJS: {}".format(str(result)))
-                return results
+                return {}
 
             if retry:
                 postUrl = urljoin(self.url, "/sample")

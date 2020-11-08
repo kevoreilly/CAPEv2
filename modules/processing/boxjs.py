8829d9423b2de5ce1e2a719a4613b07dd48b419e
doomedraven <doomedraven@users.noreply.github.com>
2020-11-08 20:16:12 +0100

    Update boxjs.py

--- modules/processing/boxjs.py
+++ modules/processing/boxjs.py
@@ -57,7 +57,7 @@ class BoxJS(Processing):
         """ Fall off if we don't deal with files """
        if self.results.get("info", {}).get("category") not in ("file", "static") and \
             (self.results.get("info", {}).get("package", "") in ("js", "jse", "jsevbe", "js_antivm") or \
-                self.results.get("target", {}).get("file", "").get("name", "").endswith(".js", ".jse") :
+                self.results.get("target", {}).get("file", "").get("name", "").endswith(".js", ".jse")):
             log.debug("Box-js supports only file scanning!")
             return {}
 

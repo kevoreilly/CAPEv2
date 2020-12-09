35c8bf3ba8a960f1bd8d5c41b09b9c36387c2f30
kfirstri <kfirstar@gmail.com>
2020-12-09 16:50:46 +0200

    add format type to get the intezer report

--- web/api/views.py
+++ web/api/views.py
@@ -1030,6 +1030,7 @@ def tasks_report(request, task_id, report_format="json"):
         "maec": "report.maec-4.1.xml",
         "maec5": "report.maec-5.0.json",
         "metadata": "report.metadata.xml",
+        "intezerreport": "../intezer/intezer-report.json"
     }
 
     bz_formats = {
@@ -1058,7 +1059,7 @@ def tasks_report(request, task_id, report_format="json"):
     if report_format.lower() in formats:
         report_path = os.path.join(CUCKOO_ROOT, "storage", "analyses", "%s" % task_id, "reports", formats[report_format.lower()])
         if os.path.exists(report_path):
-            if report_format in ("json", "maec5"):
+            if report_format in ("intezeranalyze", "json", "maec5"):
                 content = "application/json; charset=UTF-8"
                 ext = "json"
             elif report_format.startswith("html"):

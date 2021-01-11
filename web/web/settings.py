51c808b543e3cc64364772b8b465a419aa638b03
doomedraven <doomedraven@users.noreply.github.com>
2021-01-11 19:05:54 +0100

    Update settings.py

--- web/web/settings.py
+++ web/web/settings.py
@@ -134,14 +134,14 @@ MEDIA_URL = ""
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.getcwd(), "static")
+STATIC_ROOT = ""
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = "/static/"
 
 # Additional locations of static files
-STATICFILES_DIRS = () #(os.path.join(os.getcwd(), "static"),)
+STATICFILES_DIRS = (os.path.join(os.getcwd(), "static"),)
 
 # List of finder classes that know how to find static files in
 # various locations.

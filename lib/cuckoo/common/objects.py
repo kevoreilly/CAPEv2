9bdaa531d4b2986738c040eb0069cb48ec9a4ae8
doomedraven <doomedraven@users.noreply.github.com>
2021-01-20 18:04:21 +0100

    Update objects.py

--- lib/cuckoo/common/objects.py
+++ lib/cuckoo/common/objects.py
@@ -304,7 +304,8 @@ class File(object):
         self._sha512 = sha512.hexdigest()
         self._sha3_384 = sha3_384.hexdigest()
         if HAVE_TLSH:
-            self._tlsh_hash = tlsh_hash.final()
+            tlsh_hash.final()
+            self._tlsh_hash = tlsh_hash.hexdigest()
 
     @property
     def file_data(self):

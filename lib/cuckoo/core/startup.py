f2f711347bbd7c2c53b7e38ec98c04d2ba528b60
doomedraven <doommedraven@gmail.com>
2021-02-07 22:21:46 +0100

    Update startup.py

--- lib/cuckoo/core/startup.py
+++ lib/cuckoo/core/startup.py
@@ -15,7 +15,7 @@ import logging.handlers
 try:
     import yara
     if not int(yara.__version__[0]) >= 4:
-        raise("Missed library: pip3 install yara-python>=4.0.0 -U")
+        raise ImportError("Missed library: pip3 install yara-python>=4.0.0 -U")
 except ImportError:
     print("Missed library: pip3 install yara-python>=4.0.0 -U")
 import modules.auxiliary

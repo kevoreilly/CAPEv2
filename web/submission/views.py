da377b57c34eddac38157cf3af384e9474a7d564
doomedraven <doommedraven@gmail.com>
2021-02-20 11:43:04 +0100

    random_route for web_gui

--- web/submission/views.py
+++ web/submission/views.py
@@ -491,10 +491,18 @@ def index(request, resubmit_hash=False):
             socks5s_random = random.choice(socks5s.values()).get("description", False)
 
         if routing.vpn.random_vpn:
-            vpn_random =  random.choice(list(vpns.values()))
+            vpn_random =  random.choice(list(vpns.values())).get("description", False)
 
         if socks5s:
             socks5s_random = random.choice(list(socks5s.values())).get("description", False)
+
+        if vpn_random and socks5s_random:
+            random_route = random.choice((vpn_random, socks5s_random))
+        elif vpn_random:
+            random_route = vpn_random
+        elif socks5s_random:
+            random_route = socks5s_random
+
 
         existent_tasks = dict()
         if resubmit_hash:
@@ -510,9 +518,8 @@ def index(request, resubmit_hash=False):
                 "packages": sorted(packages),
                 "machines": machines,
                 "vpns": list(vpns.values()),
-                "vpn_random": vpn_random,
+                "random_route": random_route,
                 "socks5s": list(socks5s.values()),
-                "socks5s_random": socks5s_random,
                 "route": routing.routing.route,
                 "internet": routing.routing.internet,
                 "inetsim": routing.inetsim.enabled,

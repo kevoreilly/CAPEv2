f9f162840128850991f052968cd2e8b01e04cadc
Kevin O'Reilly <kevoreilly@gmail.com>
2021-03-09 21:52:35 +0000

    Update Dridex loader sig & parser

--- modules/processing/parsers/mwcp/DridexLoader.py
+++ modules/processing/parsers/mwcp/DridexLoader.py
@@ -35,6 +35,7 @@ rule DridexLoader
         $c2parse_3 = {89 08 66 39 1D ?? ?? ?? ?? A1 ?? ?? ?? ?? 0F 95 C1 88 48 04 80 3D ?? ?? ?? ?? 0A 77 05 A0 ?? ?? ?? ?? 80 3D ?? ?? ?? ?? 00 56 8B F3 76 4E 66 8B 04 F5}
         $c2parse_4 = {0F B7 C0 89 01 A0 ?? ?? ?? ?? 3C 0A 77 ?? A0 ?? ?? ?? ?? A0 ?? ?? ?? ?? 57 33 FF 84 C0 74 ?? 56 BE}
         $c2parse_5 = {0F B7 05 [4] 89 02 89 15 [4] 0F B6 15 [4] 83 FA 0A 7F 07 0F B6 05 [4] 0F B6 05 [4] 85 C0}
+        $c2parse_6 = {0F B7 53 ?? 89 10 0F B6 4B ?? 83 F9 0A 7F 03 8A 53 ?? 0F B6 53 ?? 85 D2 7E B9}
         $botnet_id = {C7 00 00 00 00 00 8D 00 6A 04 50 8D 4C ?? ?? E8 ?? ?? ?? ?? 0F B7 05}
         $rc4_key_1 = {56 52 BA [4] 8B F1 E8 [4] 8B C? 5? C3}
         $rc4_key_2 = {5? 8B ?9 52 [5-6] E8 [4] 8B C? 5? C3}
@@ -88,7 +89,10 @@ class DridexLoader(Parser):
                 elif '$rc4_key' in item[1] and not rc4_decode:
                     rc4_decode = int(item[0])
 
-        if line == "$c2parse_5":
+        if line == "$c2parse_6":
+            c2_rva = struct.unpack("i", filebuf[c2va_offset + 44 : c2va_offset + 48])[0] - image_base
+            botnet_rva = struct.unpack("i", filebuf[c2va_offset - 7 : c2va_offset - 3])[0] - image_base
+        elif line == "$c2parse_5":
             c2_rva = struct.unpack("i", filebuf[c2va_offset + 75 : c2va_offset + 79])[0] - image_base
             botnet_rva = struct.unpack("i", filebuf[c2va_offset + 3 : c2va_offset + 7])[0] - image_base
         elif line == "$c2parse_4":

d2109f989ec83d3a31b6709dc796112b79780319
doomedraven <doommedraven@gmail.com>
2021-03-24 07:58:20 +0100

    Update models.py

--- web/users/models.py
+++ web/users/models.py
@@ -14,4 +14,5 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
+    if hasattr(instance, "userprofile"):
+        instance.userprofile.save()

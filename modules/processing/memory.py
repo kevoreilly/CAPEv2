061ca51cf7e346ba6d3055fc2a5c03c6607bf99c
doomedraven <doommedraven@gmail.com>
2021-03-27 22:37:17 +0100

    Update memory.py
    latest supports compiled
    
    https://github.com/volatilityfoundation/volatility3/blob/develop/volatility3/framework/plugins/yarascan.py

--- modules/processing/memory.py
+++ modules/processing/memory.py
@@ -126,9 +126,12 @@ class VolatilityAPI(object):
             base_config_path = "plugins"
             single_location = self.memdump #"file:" + pathname2url(path)
             self.ctx.config["automagic.LayerStacker.single_location"] = single_location
+
         plugin = self.plugin_list.get(plugin_class)
         automagics = automagic.choose_automagic(self.automagics, plugin)
         constructed = plugins.construct_plugin(self.ctx, automagics, plugin, "plugins", None, None)
+        if plugin_class == "yarascan.YaraScan" and os.path.exists(yara_rules_path):
+            self.ctx.config["plugins.YaraScan.yara_compiled_file"] = yara_rules_path
         runned_plugin = constructed.run()
         json_data, error = ReturnJsonRenderer().render(runned_plugin)
         return json_data#, error
@@ -212,7 +215,6 @@ class VolatilityManager(object):
             return
 
         vol3 = VolatilityAPI(self.memfile)
-
         """
         if self.voptions.idt.enabled:
             try:
@@ -263,8 +265,6 @@ class VolatilityManager(object):
         if self.voptions.modscan.enabled:
             results["modscan"] = vol3.run("windows.modscan.ModScan")
         if self.voptions.yarascan.enabled:
-            if os.path.exists(yara_rules_path):
-                vol3.config["yara_compiled_file"] = yara_rules_path
             results["yarascan"] = vol3.run("yarascan.YaraScan")
         if self.voptions.netscan.enabled:
             results["netscan"] = vol3.run("windows.netscan.NetScan")

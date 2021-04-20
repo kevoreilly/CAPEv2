d1df6f00ca37a929777bc65a975b0f49a17d87e0
doomedraven <doommedraven@gmail.com>
2021-04-20 16:47:29 +0200

    few small fixes

--- web/web/views.py
+++ web/web/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def handler403(request, exception=None):
     return render(request, "error.html", {"error": 'Forbidden'}, status=403)
+
+def handler404(request, exception=None):
+    return redirect("/")

d1ca6857530fc99d0dfd1b5f58bba220b85583e0
doomedraven <doommedraven@gmail.com>
2021-05-08 07:29:48 +0200

    Update startup.py

--- lib/cuckoo/core/startup.py
+++ lib/cuckoo/core/startup.py
@@ -152,7 +152,7 @@ def check_linux_dist():
     ubuntu_versisons = ("18.04", "20.04")
     try:
         platform_details = platform.dist()
-        if platform_details[0] != "Ubuntu" and platform_details[1] not in ubuntu_versison:
+        if platform_details[0] != "Ubuntu" and platform_details[1] not in ubuntu_versisons:
             print(f"[!] You are using NOT supported Linux distribution by devs! Any issue report is invalid! We only support Ubuntu LTS {ubuntu_versisons}")
     except AttributeError:
         pass

476acac19adde525a5729f90d81786d60dbc849a
doomedraven <doommedraven@gmail.com>
2021-06-17 13:34:12 +0200

    fix file download, add hability to not submit huge files from VM to host

--- lib/cuckoo/common/constants.py
+++ lib/cuckoo/common/constants.py
@@ -8,7 +8,7 @@ import os
 _current_dir = os.path.abspath(os.path.dirname(__file__))
 CUCKOO_ROOT = os.path.normpath(os.path.join(_current_dir, "..", "..", ".."))
 
-ANALYSIS_BASE_PATH = os.path.join(CUCKOO_ROOT, "storage", "analyses")
+ANALYSIS_BASE_PATH = os.path.join(CUCKOO_ROOT, "storage")
 
 CUCKOO_VERSION = "2.2-CAPE"
 CUCKOO_GUEST_PORT = 8000
--- lib/cuckoo/core/scheduler.py
+++ lib/cuckoo/core/scheduler.py
@@ -190,6 +190,9 @@ class AnalysisManager(threading.Thread):
         options["enforce_timeout"] = self.task.enforce_timeout
         options["clock"] = self.task.clock
         options["terminate_processes"] = self.cfg.cuckoo.terminate_processes
+        options["upload_max_size"] = self.cfg.resultserver.upload_max_size
+        options["do_upload_max_size"] = int(self.cfg.resultserver.do_upload_max_size)
+
 
         if not self.task.timeout or self.task.timeout == 0:
             options["timeout"] = self.cfg.timeouts.default

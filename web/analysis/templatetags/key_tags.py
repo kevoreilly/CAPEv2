46ab5eb3b9aa5831dcf82a270ecf1fe5592bf0ef
DoomedRaven <doommedraven@gmail.com>
2021-06-19 17:13:56 +0200

    fix vt upload + add it to cape/procdump

--- web/analysis/templatetags/key_tags.py
+++ web/analysis/templatetags/key_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.filter(name="getkey")
 def getkey(mapping, value):
     return mapping.get(value, "")
+
+@register.filter(name="str2list")
+def str2list(value):
+    if type(value) is str:
+        return [value]
+    return value

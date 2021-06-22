28bcfc287035dda7381dab58a88214dd09430800
Kevin O'Reilly <kevoreilly@gmail.com>
2021-06-22 14:03:17 +0100

    Improve automatic detection of PowerShell scripts

--- analyzer/windows/lib/core/packages.py
+++ analyzer/windows/lib/core/packages.py
@@ -102,6 +102,8 @@ def choose_package(file_type, file_name, exports, target):
         return "inp"
     elif file_name.endswith(".vbs") or file_name.endswith(".vbe") or re.findall(br"\s?Dim\s", file_content, re.I):
         return "vbs"
+    elif b"Set-StrictMode" in file_content[:100]:
+        return "ps1"
     elif b"#@~^" in file_content[:100]:
         data = DecodeVBEJSE(file_content, "")
         if data:

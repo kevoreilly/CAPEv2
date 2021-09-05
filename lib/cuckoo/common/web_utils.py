c82a92c4651d6611cc956a1a2ff288dac6618bd6
doomedraven <doommedraven@gmail.com>
2021-09-05 12:00:38 +0200

    Update web_utils.py

--- lib/cuckoo/common/web_utils.py
+++ lib/cuckoo/common/web_utils.py
@@ -749,8 +749,10 @@ def _download_file(route, url, options):
             headers[key.replace("dne_", "")] = value
 
     try:
-        url_defag = url.replace("[.]", ".").replace("[.", ".").repalce(".]", ".").replace("hxxp", "http").replace("hxtp", "http")
-        response = requests.get(url_defag, headers=headers, proxies=proxies)
+        url_defang = url.replace("[.]", ".").replace("[.", ".").repalce(".]", ".").replace("hxxp", "http").replace("hxtp", "http")
+        if not url_defang.startswith("http"):
+            url_defang = "http://" + url_defang
+        response = requests.get(url_defang, headers=headers, proxies=proxies)
         if response and response.status_code == 200:
             return response.content
     except Exception as e:

kevoreilly/CAPEv2

commit d298cc3426ffb4b044be2a80fa044d705ff4ae48

Set maxsplit in `.split` and `.rsplit` (#680)

modules/processing/curtain.py
@@ -246,7 +246,7 @@ def formatReplace(inputString, MODFLAG):
     # obfGroup = re.search("(\"|\')(\{[0-9]{1,2}\})+(\"|\')[ -fF]+?(\"|\').+?(\"|\')(?=\)([!.\"\';)( ]))", inputString).group()
 
     # Build index and string lists
-    indexList = [int(x) for x in re.findall("\d+", obfGroup.split("-")[0])]
+    indexList = [int(x) for x in re.findall("\d+", obfGroup.split("-", 1)[0])]
 
     # This is to address scenarios where the string built is more PS commands with quotes
     stringList = re.search("(\"|').+", "-".join(obfGroup.split("-")[1:])[:-1]).group()
@@ -279,7 +279,7 @@ def charReplace(inputString, MODFLAG):
     # OLD: [char]101
     # NEW: e
     for value in re.findall("\[[Cc][Hh][Aa][Rr]\][0-9]{1,3}", inputString):
-        inputString = inputString.replace(value, f'"{chr(int(value.split("]")[1]))}"')
+        inputString = inputString.replace(value, f'"{chr(int(value.split("]", 2)[1]))}"')
     if MODFLAG == 0:
         MODFLAG = 1
     return inputString, MODFLAG
@@ -386,36 +386,36 @@ def replaceDecoder(inputString, MODFLAG):
     inputString = inputString.replace("'|'", "char[124]")
 
     if "|" in inputString:
-        if "replace" not in inputString.split("|")[-1]:
-            inputString = "|".join(inputString.split("|")[0:-1])
+        if "replace" not in inputString.rsplit("|", 1)[-1]:
+            inputString = "|".join(inputString.rsplit("|", 1)[:-1])
         else:
             pass
 
-    while "replace" in inputString.split(".")[-1].lower() or "replace" in inputString.split("-")[-1].lower():
+    while "replace" in inputString.rsplit(".", 1)[-1].lower() or "replace" in inputString.rsplit("-", 1)[-1].lower():
 
         inputString = inputString.replace("'+'", "")
         inputString = inputString.replace("'|'", "char[124]")
 
-        if len(inputString.split(".")[-1]) > len(inputString.split("-")[-1]):
+        if len(inputString.rsplit(".", 1)[-1]) > len(inputString.rsplit("-", 1)[-1]):
 
-            tempString = "-".join(inputString.split("-")[0:-1])
-            replaceString = inputString.split("-")[-1]
+            tempString = "-".join(inputString.rsplit("-", 1)[:-1])
+            replaceString = inputString.rsplit("-", 1)[-1]
 
-            if "[" in replaceString.split(",")[0]:
-                firstPart = " ".join(replaceString.split(",")[0].split("[")[1:]).replace("'", "").replace('"', "")
+            if "[" in replaceString.split(",", 1)[0]:
+                firstPart = " ".join(replaceString.split(",", 1)[0].split("[")[1:]).replace("'", "").replace('"', "")
 
-            elif "'" in replaceString.split(",")[0].strip() or '"' in replaceString.split(",")[0].strip():
-                firstPart = re.search("('.+?'|\".+?\")", replaceString.split(",")[0]).group().replace("'", "").replace('"', "")
+            elif "'" in replaceString.split(",", 1)[0].strip() or '"' in replaceString.split(",", 1)[0].strip():
+                firstPart = re.search("('.+?'|\".+?\")", replaceString.split(",", 1)[0]).group().replace("'", "").replace('"', "")
 
             else:
-                firstPart = replaceString.split(",")[0].split("'")[1].replace("'", "").replace('"', "")
+                firstPart = replaceString.split(",", 1)[0].split("'", 2)[1].replace("'", "").replace('"', "")
 
-            secondPart = replaceString.split(",")[1].split(")")[0].replace("'", "").replace('"', "")
+            secondPart = replaceString.split(",", 2)[1].split(")", 1)[0].replace("'", "").replace('"', "")
         else:
-            tempString = ".".join(inputString.split(".")[0:-1])
-            replaceString = inputString.split(".")[-1]
-            firstPart = replaceString.split(",")[0].split("(")[-1].replace("'", "").replace('"', "")
-            secondPart = replaceString.split(",")[1].split(")")[0].replace("'", "").replace('"', "")
+            tempString = ".".join(inputString.split(".")[:-1])
+            replaceString = inputString.rsplit(".", 1)[-1]
+            firstPart = replaceString.split(",", 1)[0].rsplit("(", 1)[-1].replace("'", "").replace('"', "")
+            secondPart = replaceString.split(",", 2)[1].split(")", 1)[0].replace("'", "").replace('"', "")
 
         if "+" in firstPart:
 
@@ -444,8 +444,8 @@ def replaceDecoder(inputString, MODFLAG):
         tempString = tempString.replace(firstPart, secondPart)
         inputString = tempString
 
-        if "replace" not in inputString.split("|")[-1].lower():
-            inputString = inputString.split("|")[0]
+        if "replace" not in inputString.rsplit("|", 1)[-1].lower():
+            inputString = inputString.split("|", 1)[0]
 
     if MODFLAG == 0:
         MODFLAG = 1

utils/tridupdate.py
@@ -32,7 +32,7 @@ PROGRAM_VER = "1.10"
 
 def errexit(mess, errlev=1):
     """Display an error and exit."""
-    print("%s: error: %s" % (os.path.split(sys.argv[0])[1], mess))
+    print("%s: error: %s" % (os.path.split(sys.argv[0], 2)[1], mess))
     sys.exit(errlev)
 
 

lib/cuckoo/common/abstracts.py
@@ -690,7 +690,7 @@ class Processing(object):
         self.shots_path = os.path.join(self.analysis_path, "shots")
         self.pcap_path = os.path.join(self.analysis_path, "dump.pcap")
         self.pmemory_path = os.path.join(self.analysis_path, "memory")
-        self.memory_path = get_memdump_path(analysis_path.split("/")[-1])
+        self.memory_path = get_memdump_path(analysis_path.rsplit("/", 1)[-1])
         # self.memory_path = os.path.join(self.analysis_path, "memory.dmp")
         self.network_path = os.path.join(self.analysis_path, "network")
         self.tlsmaster_path = os.path.join(self.analysis_path, "tlsmaster.txt")
@@ -784,7 +784,7 @@ class Signature(object):
         self.pcap_path = os.path.join(self.analysis_path, "dump.pcap")
         self.pmemory_path = os.path.join(self.analysis_path, "memory")
         # self.memory_path = os.path.join(self.analysis_path, "memory.dmp")
-        self.memory_path = get_memdump_path(analysis_path.split("/")[-1])
+        self.memory_path = get_memdump_path(analysis_path.rsplit("/", 1)[-1])
 
         try:
             create_folder(folder=self.reports_path)
@@ -1590,7 +1590,7 @@ class Report(object):
         self.pcap_path = os.path.join(self.analysis_path, "dump.pcap")
         self.pmemory_path = os.path.join(self.analysis_path, "memory")
         # self.memory_path = os.path.join(self.analysis_path, "memory.dmp")
-        self.memory_path = get_memdump_path(analysis_path.split("/")[-1])
+        self.memory_path = get_memdump_path(analysis_path.rsplit("/", 1)[-1])
         self.files_metadata = os.path.join(self.analysis_path, "files.json")
 
         try:

lib/cuckoo/common/cape_utils.py
@@ -81,7 +81,7 @@ if process_cfg.mwcp.enabled:
 
         logging.getLogger("mwcp").setLevel(logging.CRITICAL)
         mwcp.register_parser_directory(os.path.join(CUCKOO_ROOT, process_cfg.mwcp.modules_path))
-        malware_parsers = {block.name.split(".")[-1]: block.name for block in mwcp.get_parser_descriptions(config_only=False)}
+        malware_parsers = {block.name.rsplit(".", 1)[-1]: block.name for block in mwcp.get_parser_descriptions(config_only=False)}
         HAS_MWCP = True
         assert "MWCP_TEST" in malware_parsers
     except ImportError as e:
@@ -121,7 +121,7 @@ if process_cfg.malduck.enabled:
         malduck_rules = Yara.__new__(Yara)
         malduck_modules = ExtractorModules.__new__(ExtractorModules)
         # tmp_modules = load_modules(os.path.join(CUCKOO_ROOT, process_cfg.malduck.modules_path))
-        # malduck_modules_names = dict((k.split(".")[-1], v) for k, v in tmp_modules.items())
+        # malduck_modules_names = dict((k.rsplit(".", 1)[-1], v) for k, v in tmp_modules.items())
         malduck_modules_names = malduck_load_decoders(CUCKOO_ROOT)
         malduck_modules.extractors = Extractor.__subclasses__()
         HAVE_MALDUCK = True
@@ -194,7 +194,7 @@ def init_yara():
                 File.yara_initialized = True
                 break
             except yara.SyntaxError as e:
-                bad_rule = str(e).split(".yar")[0] + ".yar"
+                bad_rule = str(e).split(".yar", 1)[0] + ".yar"
                 log.debug(f"Trying to delete bad rule: {bad_rule}")
                 if os.path.basename(bad_rule) in indexed:
                     for k, v in rules.items():
@@ -344,7 +344,7 @@ def static_config_parsers(yara_hit, file_data):
                 error_lines = reporter.errors[0].split("\n")
                 for line in error_lines:
                     if line.startswith("ImportError: "):
-                        logging.debug("CAPE: DC3-MWCP parser: %s", line.split(": ")[1])
+                        logging.debug("CAPE: DC3-MWCP parser: %s", line.split(": ", 2)[1])
             reporter._Reporter__cleanup()
             del reporter
         except pefile.PEFormatError:

lib/cuckoo/common/cents/cents_remcos.py
@@ -43,7 +43,7 @@ def _parse_mwcp(remcos_config):
     for c in control:
         if c and c.startswith("tcp://"):
             # maxsplit here incase the passphrase includes :
-            tmp = c.replace("tcp://", "").split(":", maxsplit=2)
+            tmp = c.replace("tcp://", "").split(":", 2)
             if tmp:
                 # if we don't have a password, just add a blank one,
                 if len(tmp) == 2:

lib/cuckoo/common/objects.py
@@ -537,7 +537,7 @@ class File(object):
                     }
                 )
         except Exception as e:
-            errcode = str(e).split()[-1]
+            errcode = str(e).rsplit(maxsplit=1)[-1]
             if errcode in yara_error:
                 log.exception("Unable to match Yara signatures for %s: %s", self.file_path, yara_error[errcode])
             else:

lib/cuckoo/common/quarantine.py
@@ -722,7 +722,7 @@ def mcafee_unquarantine(f):
                         if check == "":
                             parseit = False
                         if parseit and check.startswith("OriginalName="):
-                            malname = str(check.split("\\")[-1])
+                            malname = str(check.rsplit("\\", 1)[-1])
                     if not malname:
                         malname = "McAfeeDequarantineFile"
                     # currently we're only returning the first found file in the quarantine file

lib/cuckoo/common/suricata_detection.py
@@ -99,8 +99,8 @@ def get_suricata_family(signature):
                 break
     famchecklower = famcheck.lower()
     if famchecklower.startswith("win.") and famchecklower.count(".") == 1:
-        famchecklower = famchecklower.split(".")[-1]
-        famcheck = famcheck.split(".")[-1]
+        famchecklower = famchecklower.rsplit(".", 1)[-1]
+        famcheck = famcheck.rsplit(".", 1)[-1]
     if famchecklower in ("win32", "w32", "ransomware"):
         famcheck = words[3]
         famchecklower = famcheck.lower()

commit 6cb87d4a18a1d1e7c70ec79aed062d4163a171ee

Fix bugs introduced in d298cc3426ffb4b044be2a80fa044d705ff4ae48

modules/processing/curtain.py
@@ -387,7 +387,7 @@ def replaceDecoder(inputString, MODFLAG):
 
     if "|" in inputString:
         if "replace" not in inputString.rsplit("|", 1)[-1]:
-            inputString = "|".join(inputString.rsplit("|", 1)[:-1])
+            inputString = inputString.rsplit("|", 1)[0]
         else:
             pass
 
@@ -398,7 +398,7 @@ def replaceDecoder(inputString, MODFLAG):
 
         if len(inputString.rsplit(".", 1)[-1]) > len(inputString.rsplit("-", 1)[-1]):
 
-            tempString = "-".join(inputString.rsplit("-", 1)[:-1])
+            tempString = inputString.rsplit("-", 1)[0]
             replaceString = inputString.rsplit("-", 1)[-1]
 
             if "[" in replaceString.split(",", 1)[0]:
@@ -412,7 +412,7 @@ def replaceDecoder(inputString, MODFLAG):
 
             secondPart = replaceString.split(",", 2)[1].split(")", 1)[0].replace("'", "").replace('"', "")
         else:
-            tempString = ".".join(inputString.split(".")[:-1])
+            tempString = inputString.rsplit(".", 1)[0]
             replaceString = inputString.rsplit(".", 1)[-1]
             firstPart = replaceString.split(",", 1)[0].rsplit("(", 1)[-1].replace("'", "").replace('"', "")
             secondPart = replaceString.split(",", 2)[1].split(")", 1)[0].replace("'", "").replace('"', "")

utils/tridupdate.py
@@ -32,7 +32,7 @@ PROGRAM_VER = "1.10"
 
 def errexit(mess, errlev=1):
     """Display an error and exit."""
-    print("%s: error: %s" % (os.path.split(sys.argv[0], 2)[1], mess))
+    print("%s: error: %s" % (os.path.split(sys.argv[0])[1], mess))
     sys.exit(errlev)
 
 
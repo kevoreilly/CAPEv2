8886dffe6151f3e03295f4e2fe81f9b41ce73e48
doomedraven <doommedraven@gmail.com>
2022-01-25 21:42:22 +0100

    restore

--- analyzer/windows/modules/auxiliary/curtain.py
+++ analyzer/windows/modules/auxiliary/curtain.py
@@ -7,7 +7,7 @@ from threading import Thread
 
 from lib.common.abstracts import Auxiliary
 from lib.common.results import upload_to_host
-from lib.core.config import AnalysisConfig
+from lib.core.config import Config
 
 log = logging.getLogger(__name__)
 
@@ -21,7 +21,7 @@ class Curtain(Thread, Auxiliary):
     def __init__(self, options={}, config=None):
         Thread.__init__(self)
         Auxiliary.__init__(self, options, config)
-        self.config = AnalysisConfig()
+        self.config = Config()
         self.enabled = self.config.curtain
         self.do_run = self.enabled
         self.startupinfo = subprocess.STARTUPINFO()
--- analyzer/windows/modules/auxiliary/evtx.py
+++ analyzer/windows/modules/auxiliary/evtx.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 from lib.common.abstracts import Auxiliary
 from lib.common.results import upload_to_host
-from lib.core.config import AnalysisConfig
+from lib.core.config import Config
 
 log = logging.getLogger(__name__)
 
@@ -31,7 +31,7 @@ class Evtx(Thread, Auxiliary):
     def __init__(self, options={}, config=None):
         Thread.__init__(self)
         Auxiliary.__init__(self, options, config)
-        self.config = AnalysisConfig()
+        self.config = Config()
         self.enabled = self.config.evtx
         self.do_run = self.enabled
 

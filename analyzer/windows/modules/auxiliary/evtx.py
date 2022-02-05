590806cd5c6e9b4087ab7413422c8e9a3f873b1f
doomedraven <doommedraven@gmail.com>
2022-02-05 10:37:31 +0100

    evtx missed space

--- analyzer/windows/modules/auxiliary/evtx.py
+++ analyzer/windows/modules/auxiliary/evtx.py
@@ -168,10 +168,7 @@ class Evtx(Thread, Auxiliary):
         for policy in advanced_audit_policies:
             for subcategory, settings in policy.items():
                 try:
-                    cmd = (
-                        f'auditpol /set /subcategory:"{subcategory}"'
-                        f'/success:{settings["success"]} /failure:{settings["failure"]}'
-                    )
+                    cmd = f'auditpol /set /subcategory:"{subcategory}" /success:{settings["success"]} /failure:{settings["failure"]}'
                     log.debug("Enabling advanced logging -> %s", cmd)
                     os.system(cmd)
                 except Exception as err:

30e3b93c0c92beb22a03c61c3b4aef1bff610d2b
Lint Action <lint-action@samuelmeuli.com>
2022-02-05 08:11:02 +0000

    Fix code style issues with Black

--- modules/processing/parsers/CAPE/IcedID.py
+++ modules/processing/parsers/CAPE/IcedID.py
@@ -70,7 +70,9 @@ def config(filebuf):
 
             return {}
 
+
 if __name__ == "__main__":
     import sys
+
     with open(sys.argv[1], "rb") as f:
         print(config(f.read()))
--- modules/processing/parsers/CAPE/IcedIDLoader.py
+++ modules/processing/parsers/CAPE/IcedIDLoader.py
@@ -59,5 +59,3 @@ if __name__ == "__main__":
 
     with open(sys.argv[1], "rb") as f:
         print(config(f.read()))
-
-

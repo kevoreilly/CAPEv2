0bdcc8eeabc7e559758ea4fd7b557ad136ea84ad
doomedraven <doommedraven@gmail.com>
2022-02-13 10:38:46 +0100

    peepdf is pretty buggy on py3 so moving that out and leaving it to user if they want to install it

--- modules/processing/static.py
+++ modules/processing/static.py
@@ -26,6 +26,7 @@ from lib.cuckoo.common.cape_utils import generic_file_extractors
 from lib.cuckoo.common.config import Config
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.common.icon import PEGroupIconDir
+from lib.cuckoo.common.integration.peepdf import peepdf_parse
 from lib.cuckoo.common.objects import File, IsPEImage
 from lib.cuckoo.common.pdftools.pdfid import PDFiD, PDFiD2JSON
 from lib.cuckoo.common.structures import LnkEntry, LnkHeader
@@ -56,13 +57,6 @@ try:
     HAVE_PEFILE = True
 except ImportError:
     HAVE_PEFILE = False
-
-try:
-    import v8py
-
-    HAVE_V8PY = True
-except ImportError:
-    HAVE_V8PY = False
 
 try:
     import cryptography
@@ -101,14 +95,6 @@ try:
 except ImportError:
     print("Missed oletools dependency: pip3 install oletools")
     HAVE_OLETOOLS = False
-
-try:
-    from peepdf.JSAnalysis import analyseJS
-    from peepdf.PDFCore import PDFParser
-
-    HAVE_PEEPDF = True
-except ImportError as e:
-    HAVE_PEEPDF = False
 
 try:
     from elftools.common.exceptions import ELFError
@@ -1100,64 +1086,9 @@ class PDF(object):
 
     def __init__(self, file_path):
         self.file_path = file_path
-        self.pdf = None
-        self.base_uri = ""
-
-    def _clean_string(self, value):
-        # handle BOM for typical english unicode while avoiding some
-        # invalid BOM seen in malicious PDFs (like using the utf16le BOM
-        # for an ascii string)
-        if value.startswith("\xfe\xff"):
-            clean = True
-            for x in value[2::2]:
-                if ord(x):
-                    clean = False
-                    break
-            if clean:
-                return value[3::2]
-        elif value.startswith("\xff\xfe"):
-            clean = True
-            for x in value[3::2]:
-                if ord(x):
-                    clean = False
-                    break
-            if clean:
-                return value[2::2]
-        return value
-
-    def _get_obj_val(self, version, obj):
-        try:
-            if obj.type == "reference":
-                return self.pdf.body[version].getObject(obj.id)
-        except Exception:
-            pass
-        return obj
-
-    def _set_base_uri(self):
-        try:
-            for version in range(self.pdf.updates + 1):
-                trailer, _ = self.pdf.trailer[version]
-                if trailer is not None:
-                    elem = trailer.dict.getElementByName("/Root")
-                    if elem:
-                        elem = self._get_obj_val(version, elem)
-                        if elem:
-                            elem = elem.getElementByName("/URI")
-                            if elem:
-                                elem = self._get_obj_val(version, elem)
-                                if elem:
-                                    elem = elem.getElementByName("/Base")
-                                    if elem:
-                                        elem = self._get_obj_val(version, elem)
-                                        if elem:
-                                            self.base_uri = elem.getValue()
-        except Exception as e:
-            log.error(e, exc_info=True)
-            pass
 
     def _parse(self, filepath):
-        """Parses the PDF for static information. Uses V8Py from peepdf to
-        extract JavaScript from PDF objects.
+        """Parses the PDF for static information.
         @param filepath: Path to file to be analyzed.
         @return: results dict or None.
         """
@@ -1189,110 +1120,7 @@ class PDF(object):
         pdfresult["Dates"] = dates
         pdfresult["Keywords"] = keywords
 
-        if not HAVE_PEEPDF:
-            return result
-
-        log.debug("About to parse with PDFParser")
-        parser = PDFParser()
-        ret, self.pdf = parser.parse(filepath, forceMode=True, looseMode=True, manualAnalysis=False)
-        urlset = set()
-        annoturiset = set()
-        objects = []
-        retobjects = []
-        metadata = {}
-
-        self._set_base_uri()
-
-        for i, body in enumerate(self.pdf.body):
-            metatmp = self.pdf.getBasicMetadata(i)
-            if metatmp:
-                metadata = metatmp
-
-            objects = body.objects
-
-            for index in objects:
-                oid = objects[index].id
-                offset = objects[index].offset
-                size = objects[index].size
-                details = objects[index].object
-
-                obj_data = {}
-                obj_data["Object ID"] = oid
-                obj_data["Offset"] = offset
-                obj_data["Size"] = size
-                if details.type == "stream":
-                    encoded_stream = details.encodedStream
-                    decoded_stream = details.decodedStream
-                    if HAVE_V8PY:
-                        jsdata = None
-                        try:
-                            jslist, unescapedbytes, urlsfound, errors, ctxdummy = analyseJS(decoded_stream.strip())
-                            jsdata = jslist[0]
-                        except Exception as e:
-                            log.error(e, exc_info=True)
-                            continue
-                        if len(errors):
-                            continue
-                        if jsdata is None:
-                            continue
-
-                        for url in urlsfound:
-                            urlset.add(url)
-
-                        # The following loop is required to "JSONify" the strings returned from PyV8.
-                        # As PyV8 returns byte strings, we must parse out bytecode and
-                        # replace it with an escape '\'. We can't use encode("string_escape")
-                        # as this would mess up the new line representation which is used for
-                        # beautifying the javascript code for Django's web interface.
-                        ret_data = ""
-                        for char in jsdata:
-                            if ord(char) > 127:
-                                tmp = f"\\x{char.encode().hex()}"
-                            else:
-                                tmp = char
-                            ret_data += tmp
-                    else:
-                        continue
-
-                    obj_data["Data"] = ret_data
-                    retobjects.append(obj_data)
-                elif details.type == "dictionary" and details.hasElement("/A"):
-                    # verify it to be a link type annotation
-                    subtype_elem = details.getElementByName("/Subtype")
-                    type_elem = details.getElementByName("/Type")
-                    if not subtype_elem or not type_elem:
-                        continue
-                    subtype_elem = self._get_obj_val(i, subtype_elem)
-                    type_elem = self._get_obj_val(i, type_elem)
-                    if subtype_elem.getValue() != "/Link" or type_elem.getValue() != "/Annot":
-                        continue
-                    a_elem = details.getElementByName("/A")
-                    a_elem = self._get_obj_val(i, a_elem)
-                    if a_elem.type == "dictionary" and a_elem.hasElement("/URI"):
-                        uri_elem = a_elem.getElementByName("/URI")
-                        uri_elem = self._get_obj_val(i, uri_elem)
-                        annoturiset.add(self.base_uri + uri_elem.getValue())
-                else:
-                    # can be dictionaries, arrays, etc, don't bother displaying them
-                    # all for now
-                    pass
-                    # obj_data["File Type"] = "Encoded"
-                    # obj_data["Data"] = "Encoded"
-                    # retobjects.append(obj_data)
-
-            pdfresult["JSStreams"] = retobjects
-
-        if "creator" in metadata:
-            pdfresult["Info"]["Creator"] = convert_to_printable(self._clean_string(metadata["creator"]))
-        if "producer" in metadata:
-            pdfresult["Info"]["Producer"] = convert_to_printable(self._clean_string(metadata["producer"]))
-        if "author" in metadata:
-            pdfresult["Info"]["Author"] = convert_to_printable(self._clean_string(metadata["author"]))
-
-        if len(urlset):
-            pdfresult["JS_URLs"] = list(urlset)
-        if len(annoturiset):
-            pdfresult["Annot_URLs"] = list(annoturiset)
+        pdfresult = peepdf_parse(self.file_path, pdfresult)
 
         return result
 
@@ -2638,7 +2466,7 @@ class EncodedScriptFile(object):
                 r.append(ch)
             o += 1
 
-        if (c % 2**32) != base64.b64decode(struct.unpack("=I", source[o : o + 4]))[0]:
+        if (c % 2 ** 32) != base64.b64decode(struct.unpack("=I", source[o : o + 4]))[0]:
             log.info("Invalid checksum for Encoded WSF file!")
 
         return b"".join(ch for ch in r).decode("latin-1")

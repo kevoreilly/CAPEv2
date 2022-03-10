7a81585a2590802c5f52ed4d782b78fa0541923c
doomedraven <doomedraven@users.noreply.github.com>
2022-03-10 14:19:01 +0100

    Revert "Cleanup of admin, agent and dev_utils folder (#797)"
    This reverts commit 7b3d1f70162d65d1bca8a4f1fc225f496474c9d4.

--- dev_utils/elasticsearchdb.py
+++ dev_utils/elasticsearchdb.py
@@ -8,7 +8,6 @@
 
 import datetime
 import logging
-from typing import Iterable
 
 from lib.cuckoo.common.config import Config
 
@@ -19,7 +18,7 @@ if repconf.elasticsearchdb.enabled:
     elastic_handler = Elasticsearch(
         hosts=[repconf.elasticsearchdb.host],
         port=repconf.elasticsearchdb.get("port", 9200),
-        http_auth=(repconf.elasticsearchdb.get("username"), repconf.elasticsearchdb.get("password")),
+        http_auth=(repconf.elasticsearchdb.get("username", None), repconf.elasticsearchdb.get("password", None)),
         use_ssl=repconf.elasticsearchdb.get("use_ssl", False),
         verify_certs=repconf.elasticsearchdb.get("verify_certs", False),
         timeout=60,
@@ -32,56 +31,58 @@ if repconf.elasticsearchdb.enabled:
 
 log = logging.getLogger(__name__)
 
-ANALYSIS_INDEX_MAPPING = {
-    "mappings": {
-        "properties": {
-            "info": {
-                "properties": {
-                    "started": {"type": "date"},
-                    "machine": {"properties": {"started_on": {"type": "date"}, "shutdown_on": {"type": "date"}}},
-                }
-            },
-            "network": {"properties": {"dead_hosts": {"type": "keyword"}}},
-        }
-    }
-}
 
-
-def get_daily_analysis_index() -> str:
+def get_daily_analysis_index():
     return f"{ANALYSIS_INDEX_PREFIX}{datetime.datetime.now().strftime('%Y.%m.%d')}"
 
 
-def daily_analysis_index_exists() -> bool:
+def daily_analysis_index_exists():
     return elastic_handler.indices.exists(index=get_daily_analysis_index())
 
 
-def get_daily_calls_index() -> str:
+def get_daily_calls_index():
     return f"{CALLS_INDEX_PREFIX}{datetime.datetime.now().strftime('%Y.%m.%d')}"
 
 
-def daily_calls_index_exists() -> bool:
+def daily_calls_index_exists():
     return elastic_handler.indices.exists(index=get_daily_calls_index())
 
 
-def get_query_by_info_id(task_id: str) -> dict:
+def get_query_by_info_id(task_id):
     return {"match": {"info.id": task_id}}
 
 
-def get_analysis_index() -> str:
+def get_analysis_index():
     return f"{ANALYSIS_INDEX_PREFIX}*"
 
 
-def get_calls_index() -> str:
+def get_analysis_index_mapping():
+    return {
+        "mappings": {
+            "properties": {
+                "info": {
+                    "properties": {
+                        "started": {"type": "date"},
+                        "machine": {"properties": {"started_on": {"type": "date"}, "shutdown_on": {"type": "date"}}},
+                    }
+                },
+                "network": {"properties": {"dead_hosts": {"type": "keyword"}}},
+            }
+        }
+    }
+
+
+def get_calls_index():
     return f"{CALLS_INDEX_PREFIX}*"
 
 
-def delete_analysis_and_related_calls(task_id: str):
+def delete_analysis_and_related_calls(task_id):
     analyses = elastic_handler.search(index=get_analysis_index(), query=get_query_by_info_id(task_id))["hits"]["hits"]
     if analyses:
         log.debug("Deleting analysis data for Task %s" % task_id)
         for analysis in analyses:
             analysis = analysis["_source"]
-            for process in analysis["behavior"].get("processes", []):
+            for process in analysis["behavior"].get("processes", []) or []:
                 for call in process["calls"]:
                     elastic_handler.delete_by_query(index=get_calls_index(), body={"query": {"match": {"_id": call}}})
 
@@ -89,17 +90,17 @@ def delete_analysis_and_related_calls(task_id: str):
         log.debug("Deleted previous ElasticsearchDB data for Task %s" % task_id)
 
 
-def scroll(scroll_id: str) -> dict:
+def scroll(scroll_id):
     return elastic_handler.scroll(scroll_id=scroll_id, scroll=SCROLL_TIME)
 
 
-def scroll_docs(index: str, query: dict, timeout: int = 600, _source: Iterable[str] = ()) -> dict:
+def scroll_docs(index, query, timeout=600, _source=()):
     return elastic_handler.search(
         index=index, body=query, scroll=SCROLL_TIME, size=SCROLL_SIZE, request_timeout=timeout, _source=_source
     )
 
 
-def all_docs(index: str, query: dict, _source: Iterable[str] = ()) -> dict:
+def all_docs(index, query, _source=()):
     # Scroll documents
     result_scroll = scroll_docs(index=index, query=query, _source=_source)
     hits = result_scroll["hits"]["hits"]

d99d69981054b7b1ae8c9f54fb438e674dd90e01
enzo <7831008+enzok@users.noreply.github.com>
2022-06-08 08:41:21 -0400

    Handle more mongodb insertion exceptions (#880)
    * Handle more mongodb insertion exceptions
    
    * Update report_doc.py

--- modules/reporting/report_doc.py
+++ modules/reporting/report_doc.py
@@ -106,7 +106,6 @@ def insert_calls(report, elastic_db=None, mongodb=False):
                         chunk_id = mongo_insert_one("calls", to_insert).inserted_id
                     except Exception as e:
                         chunk_id = None
-                        pass
                 elif elastic_db is not None:
                     chunk_id = elastic_db.index(index=get_daily_calls_index(), body=to_insert)["_id"]
                 else:
@@ -121,7 +120,10 @@ def insert_calls(report, elastic_db=None, mongodb=False):
         if chunk:
             to_insert = {"pid": process["process_id"], "calls": chunk}
             if mongodb:
-                chunk_id = mongo_insert_one("calls", to_insert).inserted_id
+                try:
+                    chunk_id = mongo_insert_one("calls", to_insert).inserted_id
+                except Exception as e:
+                    chunk_id = None
             elif elastic_db is not None:
                 chunk_id = elastic_db.index(index=get_daily_calls_index(), body=to_insert)["_id"]
             else:

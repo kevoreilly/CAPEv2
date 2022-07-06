d71413f94dce4ad8c9a2e44df2ca577bc2e20b28
doomedraven <doomedraven@users.noreply.github.com>
2022-07-06 21:40:44 +0200

    fix capa

--- lib/cuckoo/common/integrations/capa.py
+++ lib/cuckoo/common/integrations/capa.py
@@ -16,27 +16,29 @@ processing_conf = Config("processing")
 
 """
 from lib.cuckoo.common.integrations.capa import flare_capa_details, HAVE_FLARE_CAPA
-path = "/opt/CAPEv2/storage/binaries/da034c11f0c396f6cd11d22f833f9501dc75a33047ba3bd5870ff79e479bc004"
+path = "storage/binaries/8c4111e5ec6ec033ea32e7d40f3c36e16ad50146240dacfc3de6cf8df19e6531"
 details = flare_capa_details(path, "static", on_demand=True)
 """
 
+rules = False
 HAVE_FLARE_CAPA = False
 if processing_conf.flare_capa.enabled:
     try:
         from capa.version import __version__ as capa_version
-
         if capa_version[0] != "3":
             print("FLARE-CAPA missed, pip3 install -U flare-capa")
         else:
             import capa.main
-            import capa.render.utils as rutils
             import capa.rules
-            from capa.main import UnsupportedRuntimeError
-            from capa.render.result_document import (
-                convert_capabilities_to_result_document as capa_convert_capabilities_to_result_document,
-            )
+            import capa.engine
+            import capa.features
+            import capa.render.json
+            import capa.render.utils as rutils
+            import capa.render.default
+            import capa.render.result_document as rd
+            import capa.features.freeze.features as frzf
+            from capa.engine import *
             from capa.rules import InvalidRuleSet, InvalidRuleWithPath
-
             rules_path = os.path.join(CUCKOO_ROOT, "data", "capa-rules")
             if os.path.exists(rules_path):
                 try:
@@ -72,111 +74,108 @@ if processing_conf.flare_capa.enabled:
         print("FLARE-CAPA missed, pip3 install -U flare-capa")
 
 
-def render_meta(doc: Dict[str, Any]) -> Dict[str, Any]:
-    return {
-        "md5": doc["meta"]["sample"]["md5"],
-        "sha1": doc["meta"]["sample"]["sha1"],
-        "sha256": doc["meta"]["sample"]["sha256"],
-        "path": doc["meta"]["sample"]["path"],
-    }
+# == Render ddictionary helpers
+def render_meta(doc: rd.ResultDocument, result):
+    result["md5"] = doc.meta.sample.md5
+    result["sha1"] = doc.meta.sample.sha1
+    result["sha256"] = doc.meta.sample.sha256
+    result["path"] = doc.meta.sample.path
 
 
-def find_subrule_matches(doc: Dict[str, Any]) -> set:
+def find_subrule_matches(doc: rd.ResultDocument) -> Set[str]:
     """
     collect the rule names that have been matched as a subrule match.
     this way we can avoid displaying entries for things that are too specific.
     """
+    matches = set([])
 
-    def rec(node: dict) -> set:
-        matches = set()
-        if not node["success"]:
+    def rec(node: rd.Match):
+        if not node.success:
             # there's probably a bug here for rules that do `not: match: ...`
             # but we don't have any examples of this yet
             return
 
-        elif node["node"]["type"] == "statement":
-            for child in node["children"]:
+        elif isinstance(node.node, rd.StatementNode):
+            for child in node.children:
                 rec(child)
 
-        elif node["node"]["type"] == "feature":
-            if node["node"]["feature"]["type"] == "match":
-                matches.add(node["node"]["feature"]["match"])
-        return matches
-
-    matches = set()
+        elif isinstance(node.node, rd.FeatureNode):
+            if isinstance(node.node.feature, frzf.MatchFeature):
+                matches.add(node.node.feature.match)
 
     for rule in rutils.capability_rules(doc):
-        for node in rule["matches"].values():
-            matches = matches.union(rec(node))
+        for _, node in rule.matches:
+            rec(node)
 
     return matches
 
 
-def render_capabilities(doc: Dict[str, Any]) -> Dict[str, List[str]]:
+def render_capabilities(doc: rd.ResultDocument, result):
     """
     example::
-        {'accept command line arguments': ['host-interaction/cli'],
-            'allocate thread local storage (2 matches)': ['host-interaction/process'],
-            'check for time delay via GetTickCount': ['anti-analysis/anti-debugging/debugger-detection'],
-            'check if process is running under wine': ['anti-analysis/anti-emulation/wine'],
-            'contain a resource (.rsrc) section': ['executable/pe/section/rsrc'],
-            'write file (3 matches)': ['host-interaction/file-system/write']
+        {'CAPABILITY': {'accept command line arguments': 'host-interaction/cli',
+                'allocate thread local storage (2 matches)': 'host-interaction/process',
+                'check for time delay via GetTickCount': 'anti-analysis/anti-debugging/debugger-detection',
+                'check if process is running under wine': 'anti-analysis/anti-emulation/wine',
+                'contain a resource (.rsrc) section': 'executable/pe/section/rsrc',
+                'write file (3 matches)': 'host-interaction/file-system/write'}
         }
     """
     subrule_matches = find_subrule_matches(doc)
 
-    capability_dict = {}
+    result["CAPABILITY"] = dict()
     for rule in rutils.capability_rules(doc):
-        if rule["meta"]["name"] in subrule_matches:
+        if rule.meta.name in subrule_matches:
             # rules that are also matched by other rules should not get rendered by default.
             # this cuts down on the amount of output while giving approx the same detail.
             # see #224
             continue
 
-        count = len(rule["matches"])
+        count = len(rule.matches)
         if count == 1:
-            capability = rule["meta"]["name"]
+            capability = rule.meta.name
         else:
-            capability = f"{rule['meta']['name']} ({count} matches)"
+            capability = "%s (%d matches)" % (rule.meta.name, count)
 
-        capability_dict.setdefault(rule["meta"]["namespace"], []).append(capability)
-    return capability_dict
+        result["CAPABILITY"].setdefault(rule.meta.namespace, list())
+        result["CAPABILITY"][rule.meta.namespace].append(capability)
 
 
-def render_attack(doc: Dict[str, Any]) -> Dict[str, List[str]]:
+def render_attack(doc, result):
     """
     example::
-        {'COLLECTION': ['Input Capture::Keylogging [T1056.001]'],
+        {'ATT&CK': {'COLLECTION': ['Input Capture::Keylogging [T1056.001]'],
             'DEFENSE EVASION': ['Obfuscated Files or Information [T1027]',
                                 'Virtualization/Sandbox Evasion::System Checks '
                                 '[T1497.001]'],
             'DISCOVERY': ['File and Directory Discovery [T1083]',
                           'Query Registry [T1012]',
                           'System Information Discovery [T1082]'],
-            'EXECUTION': ['Shared Modules [T1129]']
+            'EXECUTION': ['Shared Modules [T1129]']}
         }
     """
-    attck_dict = {}
+    result["ATTCK"] = dict()
     tactics = collections.defaultdict(set)
     for rule in rutils.capability_rules(doc):
-        for attack in rule["meta"].get("att&ck", {}):
-            tactics[attack["tactic"]].add((attack["technique"], attack.get("subtechnique"), attack["id"]))
+        if not rule.meta.attack:
+            continue
+        for attack in rule.meta.attack:
+            tactics[attack.tactic].add((attack.technique, attack.subtechnique, attack.id))
 
     for tactic, techniques in sorted(tactics.items()):
         inner_rows = []
-        for technique, subtechnique, id in sorted(techniques):
+        for (technique, subtechnique, id) in sorted(techniques):
             if subtechnique is None:
-                inner_rows.append(f"{technique} {id}")
+                inner_rows.append("%s %s" % (technique, id))
             else:
-                inner_rows.append(f"{technique}::{subtechnique} {id}")
-        attck_dict.setdefault(tactic.upper(), inner_rows)
-    return attck_dict
+                inner_rows.append("%s::%s %s" % (technique, subtechnique, id))
+        result["ATTCK"].setdefault(tactic.upper(), inner_rows)
 
 
-def render_mbc(doc: Dict[str, Any]) -> Dict[str, List[str]]:
+def render_mbc(doc, result):
     """
     example::
-        {'ANTI-BEHAVIORAL ANALYSIS': ['Debugger Detection::Timing/Delay Check '
+        {'MBC': {'ANTI-BEHAVIORAL ANALYSIS': ['Debugger Detection::Timing/Delay Check '
                                       'GetTickCount [B0001.032]',
                                       'Emulator Detection [B0004]',
                                       'Virtual Machine Detection::Instruction '
@@ -185,38 +184,43 @@ def render_mbc(doc: Dict[str, Any]) -> Dict[str, List[str]]:
          'COLLECTION': ['Keylogging::Polling [F0002.002]'],
          'CRYPTOGRAPHY': ['Encrypt Data::RC4 [C0027.009]',
                           'Generate Pseudo-random Sequence::RC4 PRGA '
-                          '[C0021.004]']
+                          '[C0021.004]']}
         }
     """
-    mbc_dict = {}
+    result["MBC"] = dict()
     objectives = collections.defaultdict(set)
     for rule in rutils.capability_rules(doc):
-        for mbc in rule["meta"].get("mbc", {}):
-            objectives[mbc["objective"]].add((mbc["behavior"], mbc.get("method"), mbc["id"]))
+        if not rule.meta.mbc:
+            continue
+
+        for mbc in rule.meta.mbc:
+            objectives[mbc.objective].add((mbc.behavior, mbc.method, mbc.id))
 
     for objective, behaviors in sorted(objectives.items()):
         inner_rows = []
-        for behavior, method, id in sorted(behaviors):
+        for (behavior, method, id) in sorted(behaviors):
             if method is None:
-                inner_rows.append(f"{behavior} [{id}]")
+                inner_rows.append("%s [%s]" % (behavior, id))
             else:
-                inner_rows.append(f"{behavior}::{method} [{id}]")
-        mbc_dict.setdefault(objective.upper(), inner_rows)
-    return mbc_dict
+                inner_rows.append("%s::%s [%s]" % (behavior, method, id))
+        result["MBC"].setdefault(objective.upper(), inner_rows)
 
 
-def render_dictionary(doc: Dict[str, Any]) -> Dict[str, Any]:
-    ostream = render_meta(doc)
-    ostream["ATTCK"] = render_attack(doc)
-    ostream["MBC"] = render_mbc(doc)
-    ostream["CAPABILITY"] = render_capabilities(doc)
-
-    return ostream
-
+def render_dictionary(doc: rd.ResultDocument) -> Dict[str, Any]:
+    result: Dict[str, Any] = dict()
+    render_meta(doc, result)
+    render_attack(doc, result)
+    render_mbc(doc, result)
+    render_capabilities(doc, result)
+    return result
 
 # ===== CAPA END
+
+
+# ==== render dictionary helpers
 def flare_capa_details(file_path: str, category: str = False, on_demand=False, disable_progress=True) -> Dict[str, Any]:
-    capa_dictionary = {}
+    # load rules from disk
+    capa_output = {}
     if (
         HAVE_FLARE_CAPA
         and processing_conf.flare_capa.enabled
@@ -225,18 +229,21 @@ def flare_capa_details(file_path: str, category: str = False, on_demand=False, d
         or on_demand
     ):
         try:
-            extractor = capa.main.get_extractor(
-                file_path, "auto", capa.main.BACKEND_VIV, signatures, disable_progress=disable_progress
-            )
-            meta = capa.main.collect_metadata("", file_path, capa.main.RULES_PATH_DEFAULT_STRING, extractor)
+            # extract features and find capabilities
+            extractor = capa.main.get_extractor(file_path, "auto", capa.main.BACKEND_VIV, [], False, disable_progress=True)
             capabilities, counts = capa.main.find_capabilities(rules, extractor, disable_progress=True)
+
+            # collect metadata (used only to make rendering more complete)
+            meta = capa.main.collect_metadata([], file_path, [rules_path], extractor)
             meta["analysis"].update(counts)
-            doc = capa_convert_capabilities_to_result_document(meta, rules, capabilities)
-            capa_dictionary = render_dictionary(doc)
+            meta["analysis"]["layout"] = capa.main.compute_layout(rules, extractor, capabilities)
+            # ...as python dictionary, simplified as textable but in dictionary
+            doc = rd.ResultDocument.from_capa(meta, rules, capabilities)
+            capa_output = render_dictionary(doc)
         except MemoryError:
             log.warning("FLARE CAPA -> MemoryError")
-        except UnsupportedRuntimeError:
-            log.error("FLARE CAPA -> UnsupportedRuntimeError")
+        # except UnsupportedRuntimeError:
+        #     log.error("FLARE CAPA -> UnsupportedRuntimeError")
         except Exception as e:
             log.error(e, exc_info=True)
-    return capa_dictionary
+    return capa_output

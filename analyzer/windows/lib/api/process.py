kevoreilly/CAPEv2

commit aee63208886101be30b83ad63987e08efe9b59a6

feat: new zip_compound package (#969)
* feat: new zip_compound package

* docs: add zip_compound into packages.rst

analyzer/windows/lib/api/process.py
@@ -48,6 +48,7 @@ from lib.common.defines import (
 from lib.common.errors import get_error_string
 from lib.common.rand import random_string
 from lib.common.results import upload_to_host
+from lib.core.compound import create_custom_folders
 from lib.core.config import Config
 from lib.core.log import LogServer
 
@@ -407,8 +408,20 @@ class Process:
             self.suspended = True
             creation_flags += CREATE_SUSPENDED
 
+        # Use the custom execution directory if provided, otherwise launch in the same location
+        # where the sample resides (default %TEMP%)
+        if "executiondir" in self.options.keys():
+            execution_directory = self.options["executiondir"]
+        elif "curdir" in self.options.keys():
+            execution_directory = self.options["curdir"]
+        else:
+            execution_directory = os.getenv("TEMP")
+
+        # Try to create the custom directories so that the execution path is deemed valid
+        create_custom_folders(execution_directory)
+
         created = KERNEL32.CreateProcessW(
-            path, arguments, None, None, None, creation_flags, None, os.getenv("TEMP"), byref(startup_info), byref(process_info)
+            path, arguments, None, None, None, creation_flags, None, execution_directory, byref(startup_info), byref(process_info)
         )
 
         if created:
@@ -566,6 +579,7 @@ class Process:
                 "nohuman",
                 "main_task_id",
                 "auto",
+                "tlsdump",
                 "pre_script_args",
                 "pre_script_timeout",
                 "during_script_args",

analyzer/windows/modules/auxiliary/tlsdump.py
@@ -51,4 +51,3 @@ class TLSDumpMasterSecrets(Auxiliary):
                     "An unknown error occurred while trying to inject into the lsass.exe process to dump TLS master secrets: %s",
                     e,
                 )
-        del self.options["tlsdump"]

.pre-commit-config.yaml
@@ -3,6 +3,7 @@ repos:
     rev: v4.3.0
     hooks:
       - id: check-json
+        exclude: tests/zip_compound/files/misconfiguration.json
       - id: check-yaml
       - id: end-of-file-fixer
       - id: trailing-whitespace

analyzer/windows/analyzer.py
@@ -37,6 +37,7 @@ from lib.common.defines import ADVAPI32, EVENT_MODIFY_STATE, KERNEL32, NTDLL, SY
 from lib.common.exceptions import CuckooError, CuckooPackageError
 from lib.common.hashing import hash_file
 from lib.common.results import upload_to_host
+from lib.core.compound import create_custom_folders
 from lib.core.config import Config
 from lib.core.packages import choose_package
 from lib.core.pipe import PipeDispatcher, PipeForwarder, PipeServer, disconnect_pipes
@@ -236,6 +237,12 @@ class Analyzer:
         self.config = Config(cfg="analysis.conf")
         self.options = self.config.get_options()
 
+        # Resolve the paths first in case some other part of the code needs those (fullpath) parameters.
+        if "curdir" in self.options:
+            self.options["curdir"] = os.path.expandvars(self.options["curdir"])
+        if "executiondir" in self.options:
+            self.options["executiondir"] = os.path.expandvars(self.options["executiondir"])
+
         # Set the default DLL to be used for this analysis.
         self.default_dll = self.options.get("dll")
 
@@ -382,7 +389,11 @@ class Analyzer:
         # E.g., for some samples it might be useful to run from %APPDATA%
         # instead of %TEMP%.
         if self.config.category == "file":
+            # Try to create the folders for the cases of the custom paths other than %TEMP%
+            if "curdir" in self.options:
+                create_custom_folders(self.options["curdir"])
             self.target = self.package.move_curdir(self.target)
+        log.debug("New location of moved file: %s", self.target)
 
         # Set the DLL to that specified by package
         if self.package.options.get("dll") is not None:

analyzer/windows/lib/core/compound.py
@@ -1,0 +1,41 @@
+# Copyright (C) 2021 CSIT
+# This file is part of CAPE Sandbox - http://www.capesandbox.com
+# See the file 'docs/LICENSE' for copying permission.
+
+import json
+import logging
+import os
+from json.decoder import JSONDecodeError
+
+from lib.common.exceptions import CuckooPackageError
+
+log = logging.getLogger(__name__)
+
+
+def extract_json_data(json_directory: str, json_filename: str):
+    """Extract data from json file
+    @param json_directory: The directory where the JSON file resides
+    @param json_filename: Name of the JSON file
+    @return: Entire JSON data, or {} if there is no JSON file
+    """
+    try:
+        json_path = os.path.join(json_directory, json_filename)
+        with open(json_path) as json_config_file:
+            return json.load(json_config_file)
+    except FileNotFoundError:
+        log.warning('JSON Config File "%s" not found inside ZIP Compound', json_filename)
+        return {}
+    except JSONDecodeError as e:
+        raise CuckooPackageError(f"JSON decode error. Please check format in `{json_filename}` file") from e
+
+
+def create_custom_folders(directory_path: str):
+    """Create custom folders (recursively) given the full path."""
+    if os.path.exists(directory_path):
+        log.info("%s already exists, skipping creation", directory_path)
+    else:
+        try:
+            os.makedirs(directory_path)
+            log.info("%s created", directory_path)
+        except OSError:
+            log.error("Unable to create user-defined custom folder directory")

analyzer/windows/modules/packages/exe.py
@@ -30,6 +30,7 @@ class Exe(Package):
             newpath = os.path.join(basepath, os.path.basename(path))
             shutil.copy(path, newpath)
             path = newpath
+            self.options["executiondir"] = basepath
         if runasx86:
             # ignore the return value, user must have CorFlags.exe installed in the guest VM
             call(["CorFlags.exe", path, "/32bit+"])

analyzer/windows/modules/packages/zip_compound.py
@@ -1,0 +1,177 @@
+# Copyright (C) 2021 CSIT
+# This file is part of CAPE Sandbox - http://www.capesandbox.com
+# See the file 'docs/LICENSE' for copying permission.
+
+import logging
+import os
+import shutil
+from typing import Tuple
+
+from lib.common.abstracts import Package
+from lib.common.exceptions import CuckooPackageError
+from lib.core.compound import create_custom_folders, extract_json_data
+
+log = logging.getLogger(__name__)
+
+
+class ZipCompound(Package):
+    """Extended functionality from the zip package to process compound samples"""
+
+    PATHS = [
+        ("SystemRoot", "system32", "cmd.exe"),
+        ("SystemRoot", "system32", "wscript.exe"),
+        ("SystemRoot", "system32", "rundll32.exe"),
+        ("SystemRoot", "sysnative", "WindowsPowerShell", "v1.0", "powershell.exe"),
+        ("SystemRoot", "system32", "xpsrchvw.exe"),
+    ]
+
+    @staticmethod
+    def is_valid_extension(filename: str) -> bool:
+        """
+        Checks given filename for extensions that the zip_compound package can run.
+        @param filename: name or full path of the file.
+        @return: Boolean whether extension is recognised.
+        """
+
+        valid = (
+            ".exe",
+            ".dll",
+            ".scr",
+            ".msi",
+            ".bat",
+            ".lnk",
+            ".js",
+            ".jse",
+            ".vbs",
+            ".vbe",
+            ".wsf",
+            ".ps1",
+        )
+        return filename.endswith(valid)
+
+    def process_unzipped_contents(self, unzipped_directory: str, json_filename: str) -> Tuple[str, str]:
+        """Checks JSON to move the various files to."""
+        raw_json = extract_json_data(unzipped_directory, json_filename)
+
+        json_dst_flds = raw_json.get("path_to_extract", {})
+        target_file = raw_json.get("target_file", "")
+
+        # Enforce the requirement of having a specified file. No guessing.
+        target_file = target_file or self.options.get("file")
+        if not target_file:
+            raise CuckooPackageError("File must be specified in the JSON or the web submission UI!")
+        elif not self.is_valid_extension(target_file):
+            raise CuckooPackageError("Invalid, unsupported or no extension recognised by zip_compound package")
+
+        # In case the "file" submittion option is relative, we split here
+        target_srcdir, target_name = os.path.split(target_file)
+
+        # Note for 32bit samples: Even if JSON configutation specifies "System32",
+        # wow64 redirection will still happen
+        # Commented out since redirection-related issues should be uncommon.
+        # if is_os_64bit():
+        #     wow64 = c_ulong(0)
+        #     KERNEL32.Wow64DisableWow64FsRedirection(byref(wow64))
+
+        fin_target_path = os.path.join(unzipped_directory, target_file)
+
+        # Move files that are specified in JSON file
+        if json_dst_flds:
+            for f, dst_fld in json_dst_flds.items():
+                oldpath = os.path.join(unzipped_directory, f)
+                dst_fld = os.path.expandvars(dst_fld)
+                create_custom_folders(dst_fld)
+                # If a relative path is provided, take only the basename
+                fname = os.path.split(f)[1]
+                newpath = os.path.join(dst_fld, fname)
+
+                # We cannot just shutil.move src dirs if src name == dst name.
+                if os.path.isdir(oldpath):
+                    log.debug("Resolved Dir: %s for folder '%s'", dst_fld, fname)
+                    shutil.copytree(oldpath, newpath, dirs_exist_ok=True)
+                    shutil.rmtree(oldpath)
+                else:
+                    log.debug("Resolved Dir: %s for file '%s'", dst_fld, fname)
+                    shutil.move(oldpath, newpath)
+
+                if target_file.lower() == f.lower():
+                    fin_target_path = newpath
+                    self.options["curdir"] = dst_fld
+                    log.debug("New curdir value: %s", self.options["curdir"])
+
+        # Only runs if a relative path is given for target file
+        # Errors out if the file's containing folder is shifted
+        # before shifting the target file first.
+        if target_srcdir and not os.path.exists(fin_target_path):
+            raise CuckooPackageError(
+                "Error getting the correct path for the target file! \
+                Target file should be moved before moving its containing\
+                source folder"
+            )
+
+        log.debug("Final target name: %s", target_name)
+        log.info("Final target path: %s", fin_target_path)
+        return target_name, fin_target_path
+
+    def prepare_zip_compound(self, path: str, json_filename: str) -> Tuple[str, str]:
+        """Pre-process the submitted zip file"""
+        password = self.options.get("password")
+        if password is None:
+            log.info("No archive password provided")
+            password = b""
+
+        if "curdir" in self.options:
+            root = self.options["curdir"]
+        elif "appdata" in self.options:
+            root = os.environ["APPDATA"]
+        else:
+            root = os.environ["TEMP"]
+        create_custom_folders(root)
+
+        # Have to shift this import here because of how analyzer's Package.__subclasses__ work
+        from modules.packages.zip import Zip
+
+        z = Zip()
+        z.extract_zip(path, root, password, 0)
+
+        return self.process_unzipped_contents(root, json_filename)
+
+    def start(self, path, json_config="__configuration.json"):
+        file_name, file_path = self.prepare_zip_compound(path, json_config)
+        file_name = file_name.lower()
+
+        if file_name.endswith(".lnk"):
+            cmd_path = self.get_path("cmd.exe")
+            cmd_args = f"/c start /wait '' '{file_path}'"
+            return self.execute(cmd_path, cmd_args, file_path)
+        elif file_name.endswith(".msi"):
+            msi_path = self.get_path("msiexec.exe")
+            msi_args = f"/I '{file_path}'"
+            return self.execute(msi_path, msi_args, file_path)
+        elif file_name.endswith((".js", ".jse", ".vbs", ".vbe", ".wsf")):
+            wscript = self.get_path_app_in_path("wscript.exe")
+            wscript_args = f"'{file_path}'"
+            return self.execute(wscript, wscript_args, file_path)
+        elif file_name.endswith(".dll"):
+            rundll32 = self.get_path_app_in_path("rundll32.exe")
+            function = self.options.get("function", "#1")
+            arguments = self.options.get("arguments")
+            dllloader = self.options.get("dllloader")
+            dll_args = f"'{file_path}',{function}"
+            if arguments:
+                dll_args += f" {arguments}"
+            if dllloader:
+                newname = os.path.join(os.path.dirname(rundll32), dllloader)
+                shutil.copy(rundll32, newname)
+                rundll32 = newname
+            return self.execute(rundll32, dll_args, file_path)
+        elif file_name.endswith(".ps1"):
+            powershell = self.get_path_app_in_path("powershell.exe")
+            args = f"-NoProfile -ExecutionPolicy bypass -File '{path}'"
+            return self.execute(powershell, args, file_path)
+        else:
+            if "." not in os.path.basename(file_path):
+                new_path = f"{file_path}.exe"
+                os.rename(file_path, new_path)
+                file_path = new_path
+            return self.execute(file_path, self.options.get("arguments"), file_path)

tests/zip_compound/test_extract_path.py
@@ -1,0 +1,24 @@
+import logging
+import os
+import sys
+
+logging.basicConfig(level=logging.DEBUG)
+
+testfile_dir = os.path.dirname(__file__)
+testfile_dir = os.path.join(testfile_dir, "..", "..", "analyzer", "windows")
+sys.path.append(testfile_dir)
+
+import modules.packages.zip as zip
+
+extract_dir = os.path.expandvars("%USERPROFILE%\\Desktop")
+
+
+def test_zip_Extraction():
+    zip_obj = zip.Zip()
+    zip_obj.extract_zip("./files/test_zip.zip", extract_dir)
+
+
+def test_zip_JSON():
+    zip_obj = zip.Zip()
+    f = zip_obj.process_unzipped_contents(extract_dir, "test_executable.exe")
+    print(f)

commit ada7993cc4b140c06617562aeb2a3151544fa1b2

Fix tlsdump auxiliary module breaking analyses introduced in aee6320

analyzer/windows/lib/api/process.py
@@ -579,7 +579,6 @@ class Process:
                 "nohuman",
                 "main_task_id",
                 "auto",
-                "tlsdump",
                 "pre_script_args",
                 "pre_script_timeout",
                 "during_script_args",

analyzer/windows/modules/auxiliary/tlsdump.py
@@ -51,3 +51,4 @@ class TLSDumpMasterSecrets(Auxiliary):
                     "An unknown error occurred while trying to inject into the lsass.exe process to dump TLS master secrets: %s",
                     e,
                 )
+        del self.options["tlsdump"]
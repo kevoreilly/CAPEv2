d46110272212de2cb1621ca1f3c66e8bab37f6a1
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-07-15 22:08:13 -0500

    make poetry play nice with utils/submit.py

--- utils/submit.py
+++ utils/submit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S poetry run python3
 # Copyright (C) 2010-2015 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.

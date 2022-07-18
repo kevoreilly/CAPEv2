5551295bda2aba71843cf625eb679af4cea75512
doomedraven <doommedraven@gmail.com>
2022-07-18 21:41:35 +0200

    add env variable CAPE_AS_ROOT if set to 1 will allow to run as root

--- cuckoo.py
+++ cuckoo.py
@@ -11,7 +11,7 @@ import sys
 if sys.version_info[:2] < (3, 6):
     sys.exit("You are running an incompatible version of Python, please use >= 3.6")
 
-if os.geteuid() == 0:
+if os.geteuid() == 0 and os.getenv("CAPE_AS_ROOT", "0") != "1":
     sys.exit("Root is not allowed. You gonna break permission and other parts of CAPE. RTM!")
 
 from lib.cuckoo.common.exceptions import CuckooCriticalError, CuckooDependencyError

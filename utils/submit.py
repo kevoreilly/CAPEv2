fac7347c8447140cf0aa693cddd45a274ec6d89e
doomedraven <doommedraven@gmail.com>
2022-08-19 11:57:36 +0200

    Update submit.py

--- utils/submit.py
+++ utils/submit.py
@@ -22,9 +22,9 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
 from lib.cuckoo.common.colors import bold, green, red, yellow
 from lib.cuckoo.common.exceptions import CuckooDemuxError
 from lib.cuckoo.common.objects import File
-from lib.cuckoo.common.startup import check_user_permissions
 from lib.cuckoo.common.utils import sanitize_filename, store_temp_file, to_unicode
 from lib.cuckoo.core.database import Database
+from lib.cuckoo.core.startup import check_user_permissions
 
 check_user_permissions(os.getenv("CAPE_AS_ROOT", False))
 

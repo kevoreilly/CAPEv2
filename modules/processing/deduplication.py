d25fda8e746ee1ccd6fdc04ea284df5930d2f97e
doomedraven <doommedraven@gmail.com>
2022-08-20 13:53:16 +0200

    Update deduplication.py

--- modules/processing/deduplication.py
+++ modules/processing/deduplication.py
@@ -16,7 +16,7 @@ except ImportError:
 try:
     from PIL import Image
 
-    Image.logger.setLevel.setLevel(logging.WARNING)
+    Image.logger.setLevel(logging.WARNING)
 except ImportError:
     print("Missed dependency: pip3 install Pillow")
 

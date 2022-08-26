b6086fb8732128ab304af1fd195d10a0864cee8f
enzo <7831008+enzok@users.noreply.github.com>
2022-08-26 17:40:02 -0400

    Move playback to analysis overview

--- web/guac/urls.py
+++ web/guac/urls.py
@@ -3,5 +3,4 @@ from guac import views
 
 urlpatterns = [
     re_path(r"^(?P<task_id>\d+)/(?P<session_data>[\w=]+)/$", views.index, name="index"),
-    re_path(r"^playback/(?P<task_id>\d+)/$", views.playback, name="playback"),
 ]

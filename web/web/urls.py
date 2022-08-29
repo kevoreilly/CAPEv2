a2567ca2a97442ec1725520f7a49e461b057ccee
doomedraven <doommedraven@gmail.com>
2022-08-29 09:40:26 +0200

    small cleanup

--- web/web/urls.py
+++ web/web/urls.py
@@ -6,8 +6,7 @@ from analysis import views as analysis_views
 from dashboard import views as dashboard_views
 from django.conf import settings
 from django.conf.urls import include
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
 from django.views.generic.base import TemplateView
 

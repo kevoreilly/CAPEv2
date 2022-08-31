cf8bb6d779ca94e83a322515cb56f92fceb07ee1
Kevin O'Reilly <kevoreilly@gmail.com>
2022-08-31 16:27:19 +0100

    Edge browser package

---
+++ analyzer/windows/modules/packages/edge.py
@@ -0,0 +1,12 @@
+from lib.common.abstracts import Package
+
+class Edge(Package):
+    """Edge analysis package."""
+
+    PATHS = [
+        ("ProgramFiles", "Microsoft", "Edge", "Application", "msedge.exe"),
+    ]
+
+    def start(self, url):
+        edge = self.get_path("Microsoft Edge")
+        return self.execute(edge, f'"{url}"', url)

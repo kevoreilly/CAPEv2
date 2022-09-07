b5798360dc2931640fd8335f2caf7bb18195e6af
cccs-kevin <59843993+cccs-kevin@users.noreply.github.com>
2022-09-07 10:13:57 -0400

    Adding ps1 to exe_regex and bugfix for file_name in zip package (#1137)

--- analyzer/windows/modules/packages/Unpacker_zip.py
+++ analyzer/windows/modules/packages/Unpacker_zip.py
@@ -104,7 +104,7 @@ class Unpacker_zip(Package):
     def start(self, path):
         root = os.environ["TEMP"]
         password = self.options.get("password")
-        exe_regex = re.compile(r"(\.exe|\.scr|\.msi|\.bat|\.lnk|\.js|\.jse|\.vbs|\.vbe|\.wsf)$", flags=re.IGNORECASE)
+        exe_regex = re.compile(r"(\.exe|\.scr|\.msi|\.bat|\.lnk|\.js|\.jse|\.vbs|\.vbe|\.wsf\.ps1)$", flags=re.IGNORECASE)
         dll_regex = re.compile(r"(\.dll|\.ocx)$", flags=re.IGNORECASE)
         zipinfos = self.get_infos(path)
         self.extract_zip(path, root, password, 0)

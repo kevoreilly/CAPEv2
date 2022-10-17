ed84af63214224cdd4b4edfe4aab9c123ca9ad22
enzo <7831008+enzok@users.noreply.github.com>
2022-10-17 12:45:31 -0400

    Integrate lznt1 - MalDuck is optional (#1206)

--- modules/processing/parsers/CAPE/Blister.py
+++ modules/processing/parsers/CAPE/Blister.py
@@ -8,16 +8,17 @@
 import binascii
 import json
 import logging
+import pefile
 import os
 import sys
+import yara
 from optparse import OptionParser
 from struct import pack, unpack
 
-import pefile
-import yara
-from malduck import lznt1
+from lib.cuckoo.common.integrations.lznt1 import lznt1
 
 log = logging.getLogger(__name__)
+
 
 # https://github.com/Robin-Pwner/Rabbit-Cipher/
 def ROTL8(v, n):

89787aa48f9c303970b970d8c335b4e09cabe295
doomedraven <doomedraven@users.noreply.github.com>
2023-01-01 09:53:31 +0100

    Update CAPE.py

--- modules/processing/CAPE.py
+++ modules/processing/CAPE.py
@@ -290,16 +290,15 @@ class CAPE(Processing):
         executed_config_parsers = collections.defaultdict(set)
         for tmp_path, tmp_data, hit in all_files:
             # Check for a payload or config hit
+            cape_name = None
             try:
                 if File.yara_hit_provides_detection(hit):
                     file_info["cape_type"] = hit["meta"]["cape_type"]
                     cape_name = File.get_cape_name_from_yara_hit(hit)
                     cape_names.add(cape_name)
-                else:
-                    cape_name = None
+
             except Exception as e:
                 log.error(f"Cape type error: {e}")
-                cape_name = None
             type_strings = file_info["type"].split()
             if "-bit" not in file_info["cape_type"]:
                 if type_strings[0] in ("PE32+", "PE32"):

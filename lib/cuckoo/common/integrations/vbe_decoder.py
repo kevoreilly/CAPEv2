13a4ee888cf74d31c608637ef0d43673b24b29ee
doomedraven <doomedraven@users.noreply.github.com>
2023-01-06 05:32:53 +0100

    Update vbe_decoder.py

--- lib/cuckoo/common/integrations/vbe_decoder.py
+++ lib/cuckoo/common/integrations/vbe_decoder.py
@@ -267,12 +267,16 @@ def decode_files(files: List[str]) -> str:
     return "\n".join(decode_file(file) for file in files)
 
 
-def decode_file(file: str, contents=False) -> str:
+def decode_file(file: str, contents:bytes=False) -> str:
     if not contents:
         try:
             contents = Path(file).read_bytes().decode("latin-1", errors="ignore")
         except Exception as e:
             fatal_error(f"{e.message}")
+
+    else:
+        if isinstance(contents, bytes):
+            contents = contents.decode("latin-1", errors="ignore")
 
     encoded_data = re.findall(r"#@~\^......==(.+)......==\^#~@", contents)
     return "\n".join(decode_data(data) for data in encoded_data)

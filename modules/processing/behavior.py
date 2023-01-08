7c392ffb963e519b9c9ee2b5045a829ec17fb28c
enzo <7831008+enzok@users.noreply.github.com>
2023-01-08 08:14:38 -0500

    Fix KeyError (#1304)
    * Fix KeyError
    
    fix condition where raw_value key may not exist, also raw_value_string was introduced, not sure if this also needs to be checked here, but added just in case

--- modules/processing/behavior.py
+++ modules/processing/behavior.py
@@ -262,7 +262,11 @@ class ParseProcessLog(list):
             while ent:
                 # remove the values we don't want to encode in reports
                 for arg in ent["arguments"]:
-                    del arg["raw_value"]
+                    with suppress(KeyError):
+                        if "raw_value" in arg:
+                            del arg["raw_value"]
+                        elif "raw_value_string" in arg:
+                            del arg["raw_value_string"]
                 idx += 1
                 ent = self.api_call_cache[idx]
 

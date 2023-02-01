4fb81adc7397df5710bd51ef37acc424571aa5de
doomedraven <doommedraven@gmail.com>
2023-02-01 15:40:31 +0100

    Update mitre.py

--- lib/cuckoo/common/integrations/mitre.py
+++ lib/cuckoo/common/integrations/mitre.py
@@ -3,34 +3,53 @@
 # See the file 'docs/LICENSE' for copying permission.
 
 import os
+import logging
 
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 
+log = logging.getLogger("mitre")
 
-def mitre_update():
-    """Urls might change, for proper urls see https://github.com/swimlane/pyattck"""
+def init_mitre_attck(online: bool = False):
+    config = False
+    mitre = False
+
     try:
         from pyattck import Attck
+        if online:
+            from pyattck.configuration import Configuration
+
+            config = Configuration()
     except ImportError:
         print("Missed dependency: install pyattck library, see requirements for proper version")
         return
 
-    mitre = Attck(
-        nested_techniques=True,
-        use_config=False,
-        save_config=False,
-        config_file_path=os.path.join(CUCKOO_ROOT, "data", "mitre", "config.yml"),
-        data_path=os.path.join(CUCKOO_ROOT, "data", "mitre"),
-        enterprise_attck_json="https://swimlane-pyattck.s3.us-west-2.amazonaws.com/merged_enterprise_attck_v1.json",
-        pre_attck_json="https://swimlane-pyattck.s3.us-west-2.amazonaws.com/merged_pre_attck_v1.json",
-        mobile_attck_json="https://swimlane-pyattck.s3.us-west-2.amazonaws.com/merged_mobile_attck_v1.json",
-        ics_attck_json="https://swimlane-pyattck.s3.us-west-2.amazonaws.com/merged_ics_attck_v1.json",
-        nist_controls_json="https://swimlane-pyattck.s3.us-west-2.amazonaws.com/merged_nist_controls_v1.json",
-        generated_nist_json="https://swimlane-pyattck.s3.us-west-2.amazonaws.com/attck_to_nist_controls.json",
-    )
+    try:
+        mitre = Attck(
+            nested_techniques=True,
+            use_config=False,
+            save_config=False,
+            config_file_path=os.path.join(CUCKOO_ROOT, "data", "mitre", "config.yml"),
+            data_path=os.path.join(CUCKOO_ROOT, "data", "mitre"),
+            enterprise_attck_json=config.enterprise_attck_json if online else os.path.join(CUCKOO_ROOT, "data", "mitre", "enterprise_attck_json.json"),
+            pre_attck_json=config.pre_attck_json if online else os.path.join(CUCKOO_ROOT, "data", "mitre", "pre_attck_json.json"),
+            mobile_attck_json=config.mobile_attck_json if online else os.path.join(CUCKOO_ROOT, "data", "mitre", "mobile_attck_json.json"),
+            ics_attck_json=config.ics_attck_json if online else os.path.join(CUCKOO_ROOT, "data", "mitre", "ics_attck_json.json"),
+            nist_controls_json=config.nist_controls_json if online else os.path.join(CUCKOO_ROOT, "data", "mitre", "nist_controls_json.json"),
+            generated_nist_json=config.generated_nist_json if online else os.path.join(CUCKOO_ROOT, "data", "mitre", "generated_nist_json.json"),
+        )
+    except Exception as e:
+        log.error("Can't initialize mitre's Attck class: %s", str(e))
 
-    print("[+] Updating MITRE datasets")
-    mitre.update()
+    return mitre
+
+
+def mitre_update():
+    """Urls might change, for proper urls see https://github.com/swimlane/pyattck"""
+
+    mitre = init_mitre_attck(online=True)
+    if mitre:
+        print("[+] Updating MITRE datasets")
+        mitre.update()
 
 
 def load_mitre(enabled: bool = False):
@@ -42,17 +61,14 @@ def load_mitre(enabled: bool = False):
         return mitre, HAVE_MITRE, pyattck_version
 
     try:
-        from pyattck import Attck
-
         # Till fix https://github.com/swimlane/pyattck/pull/129
         from pyattck.configuration import Configuration, Options
         from pyattck.utils.exceptions import UnknownFileError
         from pyattck.utils.version import __version_info__ as pyattck_version
 
         _ = Options._read_from_disk
-        import json  # noqa: E401
+        import json
         import warnings
-
         import yaml
 
         def _read_from_disk(self, path):
@@ -75,19 +91,7 @@ def load_mitre(enabled: bool = False):
 
         Options._read_from_disk = _read_from_disk
 
-        mitre = Attck(
-            nested_techniques=True,
-            use_config=True,
-            save_config=True,
-            config_file_path=os.path.join(CUCKOO_ROOT, "data", "mitre", "config.yml"),
-            data_path=os.path.join(CUCKOO_ROOT, "data", "mitre"),
-            enterprise_attck_json=os.path.join(CUCKOO_ROOT, "data", "mitre", "enterprise_attck_json.json"),
-            pre_attck_json=os.path.join(CUCKOO_ROOT, "data", "mitre", "pre_attck_json.json"),
-            mobile_attck_json=os.path.join(CUCKOO_ROOT, "data", "mitre", "mobile_attck_json.json"),
-            ics_attck_json=os.path.join(CUCKOO_ROOT, "data", "mitre", "ics_attck_json.json"),
-            nist_controls_json=os.path.join(CUCKOO_ROOT, "data", "mitre", "nist_controls_json.json"),
-            generated_nist_json=os.path.join(CUCKOO_ROOT, "data", "mitre", "generated_nist_json.json"),
-        )
+        mitre = init_mitre_attck()
         HAVE_MITRE = True
 
     except ImportError:

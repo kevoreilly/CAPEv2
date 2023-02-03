40115450c1b8c937e3b8c0237d6f98b4c0041669
Goh Ee Sheng <77826239+goheesheng@users.noreply.github.com>
2023-02-03 10:07:56 +0800

    Update key_tags.py

--- web/analysis/templatetags/key_tags.py
+++ web/analysis/templatetags/key_tags.py
@@ -8,25 +8,6 @@ def getkey(mapping, value):
     if isinstance(mapping, dict):
         return mapping.get(value, "")
 
-#Added: Added function to get type of variable
-@register.filter(name="gettype")
-def gettype(value):
-    return str(type(value))
-
-#Added: Added function to split list
-@register.filter(name="split")
-def split(value, key):
-    return value.split(key)
-
-#Added: Added function to search for keys in nested dictionary
-@register.filter(name="findkey")
-def findkey(value, key):
-    if key in value: return value[key]
-    for k, v in value.items():
-        if isinstance(v,dict):
-            item = findkey(v, key)
-            if item is not None:
-                return item
 
 @register.filter(name="str2list")
 def str2list(value):

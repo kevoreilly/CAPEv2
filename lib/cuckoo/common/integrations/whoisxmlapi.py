ffce5cc5a723a31a343325ffd792ab83b588952b
doomedraven <doommedraven@gmail.com>
2023-02-07 15:41:07 +0100

    Update whoisxmlapi.py

--- lib/cuckoo/common/integrations/whoisxmlapi.py
+++ lib/cuckoo/common/integrations/whoisxmlapi.py
@@ -6,8 +6,8 @@ log = logging.getLogger()
 
 def whoisxmlapi_lookup(host, apikey):
     result = {}
-    log.debug("Performing WHOIS Query for IP/Domain: %s", ip)
-    url = f"https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey={host}&domainName={ip}&outputFormat=json"
+    log.debug("Performing WHOIS Query for IP/Domain: %s", host)
+    url = f"https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey={apikey}&domainName={host}&outputFormat=json"
     try:
         r = requests.get(url, verify=False)
         if r.ok:

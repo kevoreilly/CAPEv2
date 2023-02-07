2cb37cf72add3b123357616dc8659c2a888548e2
Goh Ee Sheng <77826239+goheesheng@users.noreply.github.com>
2023-02-07 16:52:58 +0800

    Update key_tags.py

--- web/analysis/templatetags/key_tags.py
+++ web/analysis/templatetags/key_tags.py
@@ -18,16 +18,6 @@ def gettype(value):
 def split(value, key):
     return value.split(key)
 
-#Added: Added function to search for keys in nested dictionary
-@register.filter(name="findkey")
-def findkey(value, key):
-    if key in value: return value[key]
-    for k, v in value.items():
-        if isinstance(v,dict):
-            item = findkey(v, key)
-            if item is not None:
-                return item
-
 @register.filter(name="str2list")
 def str2list(value):
     if isinstance(value, str):

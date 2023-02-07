8080a8cbad2cb33dcd71cf7f4628e406dee1c68f
goheesheng <goheesheng@hotmail.com>
2023-02-07 09:47:55 +0800

    Delete to add latest version

--- analyzer/windows/modules/packages/ollydbg.py
+++
@@ -1,9 +0,0 @@
-from lib.common.abstracts import Package
-
-
-class OllyDbg(Package):
-    """OllyDbg analysis package."""
-
-    def start(self, path):
-        arguments = self.options.get("arguments", "")
-        return self.execute("bin\\OllyDbg\\OLLYDBG.EXE", f"{path} {arguments}", path)

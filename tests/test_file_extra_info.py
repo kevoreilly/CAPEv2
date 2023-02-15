1c858daa18fe55ccfb17c1e7e9adb699d6501a3e
doomedraven <doomedraven@users.noreply.github.com>
2023-02-16 00:33:59 +0100

    Update test_file_extra_info.py

--- tests/test_file_extra_info.py
+++ tests/test_file_extra_info.py
@@ -2,10 +2,11 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 import unittest
+
 from lib.cuckoo.common.integrations import file_extra_info
 
 
-class TestMsiExtract(unittest.TestCase):
+class TestFileExtraInfo(unittest.TestCase):
     def test_msi_extract(self):
         extracted_files = file_extra_info.msi_extract(
             file="tests/data/selfextraction/0ea5e25b12ab314bc9a0569c3ca756f205f40b792119f8e0fc62c874628dfea0.msi",

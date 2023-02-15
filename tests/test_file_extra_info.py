2ff2b8e1a61fdbeaf81ee9603f9f2e598ca9aac3
doomedraven <doomedraven@users.noreply.github.com>
2023-02-15 23:22:02 +0100

    Update test_file_extra_info.py

--- tests/test_file_extra_info.py
+++ tests/test_file_extra_info.py
@@ -2,16 +2,15 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 import unittest
-import os
 from lib.cuckoo.common.integrations import file_extra_info
 
 
 class TestMsiExtract(unittest.TestCase):
     def test_msi_extract(self):
-        print(os.listdir("tests/data/selfextraction"))
         extracted_files = file_extra_info.msi_extract(
             file="tests/data/selfextraction/0ea5e25b12ab314bc9a0569c3ca756f205f40b792119f8e0fc62c874628dfea0.msi",
             filetype="MSI Installer",
             **{"test": True, "options": {}}
         )
+        print(extracted_files)
         self.assertEqual(len(extracted_files["result"]["extracted_files"]), 4, "Failed to extract.")

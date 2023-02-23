54fc5ec64e4545f3a0abe4d93e8440dda3069acd
cccs-kevin <59843993+cccs-kevin@users.noreply.github.com>
2023-02-23 15:57:24 -0500

    Using pipes instead of commas since get_options cannot handle comma d… (#1392)
    * Adding community flag for pulling only YARA (#1391)
    
    * Update .yara-ci.yml
    
    * Using pipes instead of commas since get_options cannot handle comma delimiters per option

--- utils/community.py
+++ utils/community.py
@@ -104,6 +104,7 @@ def install(enabled, force, rewrite, filepath: str = False, access_token=None, p
         "data": "data",
         "integrations": "lib/cuckoo/common/integrations",
         "mitre": "data/mitre",
+        "yara": "data/yara",
     }
 
     members = t.getmembers()
@@ -199,6 +200,7 @@ def main():
         "-t", "--token", help="Access token to download private repositories", action="store", default=None, required=False
     )
     parser.add_argument("--proxy", help="Proxy to use. Ex http://127.0.0.1:8080", action="store", required=False)
+    parser.add_argument("-y", "--yara", help="Download YARA rules", action="store_true", required=False)
     args = parser.parse_args()
 
     enabled = []
@@ -219,8 +221,11 @@ def main():
             enabled.append("machinery")
         if args.analyzer:
             enabled.append("analyzer")
+        # Data contains yara
         if args.data:
             enabled.append("data")
+        elif args.yara:
+            enabled.append("yara")
         if args.integrations:
             enabled.append("integrations")
         if args.mitre_offline:

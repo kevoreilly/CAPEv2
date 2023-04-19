32e7be9f496b3ef3e7857cace9f9a4c33f96a67b
cccs-mog <117194682+cccs-mog@users.noreply.github.com>
2023-04-19 20:27:19 +0000

    Fixing merging problems

--- analyzer/windows/modules/auxiliary/human.py
+++ analyzer/windows/modules/auxiliary/human.py
@@ -206,7 +206,6 @@ def get_document_window_click_around(hwnd, lparm):
         text = create_unicode_buffer(1024)
         USER32.GetWindowTextW(hwnd, text, 1024)
         if any(value in text.value for value in ("Microsoft Word", "Microsoft Excel", "Microsoft PowerPoint", "Adobe", "Acrobat DC", "Acrobat", "Reader", "PDF")):
-        if any(value in text.value for value in ("Microsoft Word", "Microsoft Excel", "Microsoft PowerPoint", "Adobe", "Acrobat DC", "Acrobat", "Reader", "PDF")):
             USER32.SetForegroundWindow(hwnd)
             # first click the middle
             USER32.SetCursorPos(RESOLUTION["x"] // 2, RESOLUTION["y"] // 2)
@@ -260,7 +259,6 @@ def get_document_window(hwnd, lparam):
     if USER32.IsWindowVisible(hwnd):
         text = create_unicode_buffer(1024)
         USER32.GetWindowTextW(hwnd, text, 1024)
-        if any(value in text.value for value in ("- Microsoft", "- Word", "- Excel", "- PowerPoint", "- Adobe", "- Acrobat DC", "- Acrobat", "- Reader", "- PDF")):
         if any(value in text.value for value in ("- Microsoft", "- Word", "- Excel", "- PowerPoint", "- Adobe", "- Acrobat DC", "- Acrobat", "- Reader", "- PDF")):
             # send ALT+F4 equivalent
             log.info("Closing document window")

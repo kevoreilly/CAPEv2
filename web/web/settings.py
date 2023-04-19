3dfe70c65b51bc012eece0081ebc8b48fa4ac71e
doomedraven <doomedraven@users.noreply.github.com>
2023-04-19 21:13:47 +0200

    Update settings.py

--- web/web/settings.py
+++ web/web/settings.py
@@ -37,7 +37,7 @@ api_cfg = Config("api")
 # CSRF TRUSTED ORIGINS
 # For requests that include the Origin header, Django’s CSRF protection
 # requires that header match the origin present in the Host header.
-CSRF_TRUSTED_ORIGINS = [f"http://{web_cfg.general.hostname}"]
+CSRF_TRUSTED_ORIGINS = [f"http://{web_cfg.general.hostname}", f"https://{web_cfg.general.hostname}"]
 # CSRF_COOKIE_SECURE = "True"
 
 # If reporting is enabled via the web GUI, then require one of these to be enabled

5f34adb5545aa174d29ac7d551e4e9a2d90a7c7e
doomedraven <doommedraven@gmail.com>
2023-06-01 21:35:55 +0200

    Update test_bumblebee.py

--- tests_parsers/test_bumblebee.py
+++ tests_parsers/test_bumblebee.py
@@ -12,7 +12,6 @@ The idea is to have folder with samples that is scanned with yara and automatica
 
 """
 def test_bumblebee():
-    #with open("tests/data/malware/f8a6eddcec59934c42ea254cdd942fb62917b5898f71f0feeae6826ba4f3470d", "rb") as data:
-    with open("tests/data/CAPE-TestFiles/malware/f8a6eddcec59934c42ea254cdd942fb62917b5898f71f0feeae6826ba4f3470d", "rb") as data:
+    with open("tests/data/malware/f8a6eddcec59934c42ea254cdd942fb62917b5898f71f0feeae6826ba4f3470d", "rb") as data:
         conf = extract_config(data.read())
         assert conf == {"Botnet ID": "YTBSBbNTWU", "Campaign ID": "1904r", "Data": "XNgHUGLrCD", "C2s": ["444"]}

5e62b5920527a3d958be47b21da9e799566fe758
doomedraven <doommedraven@gmail.com>
2023-06-01 10:34:59 +0200

    small fix

--- lib/cuckoo/common/cape_utils.py
+++ lib/cuckoo/common/cape_utils.py
@@ -287,7 +287,7 @@ def static_config_parsers(cape_name, file_path, file_data):
                     cape_config[cape_name].update({key: [value]})
                 parser_loaded = True
         except Exception as e:
-            log.error("CAPE: parsing error on %s with %s: %s", file_path, cape_name, e)
+            log.error("CAPE: parsing error on %s with %s: %s", file_path, cape_name, e, exc_info=True)
 
     # DC3-MWCP
     if HAS_MWCP and not parser_loaded and cape_name and cape_name in malware_parsers:

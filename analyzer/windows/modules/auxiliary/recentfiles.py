82c128c298f9cd20f0b95ceb88284084590ea069
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2023-06-19 18:49:55 +0000

    Final touches on recentfiles aux

--- analyzer/windows/modules/auxiliary/recentfiles.py
+++ analyzer/windows/modules/auxiliary/recentfiles.py
@@ -6,15 +6,16 @@ import ctypes
 import logging
 import os
 import random
-import uuid
+from uuid import UUID
 
 from lib.common.abstracts import Auxiliary
 from lib.common.defines import SHELL32, SHARD_PATHA, PWSTR
-from lib.common.exceptions import CuckooError
 from lib.common.rand import random_string
 from lib.common.registry import set_regkey_full
+from lib.core.config import Config
 
 log = logging.getLogger(__name__)
+
 
 class RecentFiles(Auxiliary):
     """Populate the Desktop with recent files in order to combat recent
@@ -30,6 +31,11 @@ class RecentFiles(Auxiliary):
         "downloads": "{374DE290-123F-4565-9164-39C4925E467B}",
     }
 
+    def __init__(self, options, config):
+        Auxiliary.__init__(self, options, config)
+        self.config = Config(cfg="analysis.conf")
+        self.enabled = self.config.recentfiles
+
     def get_path(self):
         location = self.options.get("recentfiles", "documents")
         if location not in self.locations:
@@ -41,7 +47,7 @@ class RecentFiles(Auxiliary):
 
         dirpath = PWSTR()
         r = SHELL32.SHGetKnownFolderPath(
-            uuid.UUID(self.locations[location]).get_bytes_le(),
+            UUID(self.locations[location]).bytes_le,
             0, None, ctypes.byref(dirpath)
         )
         if r:
@@ -52,20 +58,25 @@ class RecentFiles(Auxiliary):
         return dirpath.value
 
     def start(self):
+        if not self.enabled:
+            return
+
         dirpath = self.get_path()
         if not dirpath:
             return
 
-        for idx in xrange(random.randint(5, 10)):
+        for idx in range(random.randint(5, 10)):
             filename = random_string(10, random.randint(10, 20))
             ext = random.choice(self.extensions)
             filepath = os.path.join(dirpath, "%s.%s" % (filename, ext))
             open(filepath, "wb").write(os.urandom(random.randint(30, 999999)))
+            log.debug("Wrote 'recentfile' %s to disk." % filepath)
 
             SHELL32.SHAddToRecentDocs(SHARD_PATHA, filepath)
 
             set_regkey_full(
-                "HKEY_CURRENT_USER\\Software\\Microsoft\\Office\\12.0\\"
+                "HKEY_CURRENT_USER\\Software\\Microsoft\\Office\\14.0\\"
                 "Word\\File MRU\\Item %d" % (idx + 1),
                 "REG_SZ", "[F00000000][T01D1C40000000000]*%s" % filepath,
             )
+

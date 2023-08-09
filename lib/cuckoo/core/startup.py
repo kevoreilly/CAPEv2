14b743985d40e1fb42125f69b84862f606bcda7d
enzo <7831008+enzok@users.noreply.github.com>
2023-08-09 08:54:24 -0400

    Remove sudo error message

--- lib/cuckoo/core/startup.py
+++ lib/cuckoo/core/startup.py
@@ -462,7 +462,10 @@ def check_tcpdump_permissions():
     pcap_permissions_error = False
     if user:
         try:
-            subprocess.check_call(["/usr/bin/sudo", "--list", "--non-interactive", tcpdump])
+            subprocess.check_call(
+                ["/usr/bin/sudo", "--list", "--non-interactive", tcpdump],
+                stderr=open('/dev/null', 'w')
+            )
         except (FileNotFoundError, subprocess.CalledProcessError):
             try:
                 if user not in grp.getgrnam("pcap").gr_mem:

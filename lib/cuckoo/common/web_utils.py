c7fba1868ecfebbdce0f1ca8ed4a5c9413684503
doomedraven <doommedraven@gmail.com>
2023-08-18 08:40:46 +0200

    Update web_utils.py

--- lib/cuckoo/common/web_utils.py
+++ lib/cuckoo/common/web_utils.py
@@ -501,6 +501,8 @@ def recon(
     if not isinstance(filename, str):
         filename = bytes2str(filename)
 
+    lowered_filename = filename.lower()
+
     if web_cfg.general.yara_recon:
         hits = File(filename).get_yara("binaries")
         for hit in hits:
@@ -515,7 +517,7 @@ def recon(
             if "package" in parsed_options:
                 package = parsed_options["package"]
 
-    if "name" in filename.lower():
+    if "name" in lowered_filename:
         orig_options += ",timeout=400,enforce_timeout=1,procmemdump=1,procdump=1"
         timeout = 400
         enforce_timeout = True

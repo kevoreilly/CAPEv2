b12a4bcc08221e9786a1f5e795b796978bbb7d22
doomedraven <doommedraven@gmail.com>
2023-10-02 14:13:21 +0200

    Update database.py

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -1543,7 +1543,7 @@ class Database(object, metaclass=Singleton):
                 package = "archive"
             elif tmp_package in ("zip", "rar"):
                 package = ""
-            elif tmp_package in ("html"):
+            elif tmp_package in ("html",):
                 package = web_conf.url_analysis.package
             else:
                 package = tmp_package

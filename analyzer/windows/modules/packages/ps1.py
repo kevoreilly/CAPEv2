b7043f186eea22b58729a6d3fd3206a6ac2fed75
Robin Koumis <rkoumis@secureworks.com>
2023-10-03 14:06:12 -0400

    Make PowerShell Core optional.
    Previously it would default to PowerShell Core (pwsh.exe) if available.
    With this change, powershell.exe is the default. Submitters can use
    the option 'pwsh' to select PowerShell Core.

--- analyzer/windows/modules/packages/ps1.py
+++ analyzer/windows/modules/packages/ps1.py
@@ -9,13 +9,25 @@ from lib.common.common import check_file_extension
 class PS1(Package):
     """PowerShell analysis package."""
 
-    PATHS = [
-        # PS v7
+    POWERSHELL_CORE = [
+        # PS v7 (Powershell Core) optional.
         ("ProgramFiles", "PowerShell", "*", "pwsh.exe"),
+    ]
+    PATHS = [
         # PS <= 5
         ("SystemRoot", "sysnative", "WindowsPowerShell", "v*.0", "powershell.exe"),
         ("SystemRoot", "system32", "WindowsPowerShell", "v*.0", "powershell.exe"),
     ]
+
+    def get_paths(self):
+        """Return list of paths to search for the PowerShell executable.
+
+        If the user selected the option, insert PowerShell Core path at the start of
+        the list.
+        """
+        if self.options.get("pwsh"):
+            return self.POWERSHELL_CORE + self.PATHS
+        return self.PATHS
 
     def start(self, path):
         powershell = self.get_path_glob("PowerShell")
---
+++ analyzer/windows/tests/modules/packages/test_ps1.py
@@ -0,0 +1,22 @@
+import unittest
+from modules.packages.ps1 import PS1
+
+
+class TestPS1(unittest.TestCase):
+
+    def test_get_paths(self):
+        """By default, the first path should be powershell.exe"""
+        ps1_module = PS1()
+        paths = ps1_module.get_paths()
+        assert paths[0][-1] == "powershell.exe"
+        all_paths = set([path[-1] for path in paths])
+        assert "pwsh.exe" not in all_paths
+
+    def test_get_paths_powershell_core(self):
+        """When option pwsh selected, the first path should be pwsh.exe"""
+        options = {"pwsh": True}
+        ps1_module = PS1(options=options)
+        paths = ps1_module.get_paths()
+        assert paths[0][-1] == "pwsh.exe"
+        all_paths = set([path[-1] for path in paths])
+        assert "powershell.exe" in all_paths

e56aceaec7359037818b4015c771fededf1622a2
winson0123 <79405382+winson0123@users.noreply.github.com>
2023-10-09 10:47:35 +0800

    refractor: strace argument parsing on frontend

--- modules/processing/strace.py
+++ modules/processing/strace.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import json
 import os
 import strace_process_tree as stp
 
@@ -11,51 +12,79 @@ log = logging.getLogger(__name__)
 __author__ = "@winson0123"
 __version__ = "1.0.0"
 
-def log_concat(unfinished, resumed):
-    """
-    Concatenates all the respective unfinished and resumed strace logs into a string,
-    matching '<unfinished ...>' and '<... {syscall} resumed>' strings accordingly,
-    returns the `resumed` time as that is the completed syscall time.
-    """
-    data = ""
-    for head in unfinished:
-        for tail in resumed:
-            if head.group("pid") != tail.group("pid") or head.group("syscall") != tail.group("syscall"):
-                continue
-            data += tail.group("pid") + ' ' + tail.group("time") + ' ' + head.group("unfinished") + tail.group("resumed") + '\n'
-            resumed.remove(tail)
-            unfinished.remove(head)
-            break
-    return data
+class Processes():
+    """Processes analyzer."""
 
-def parse_log(strace_logs):
-    results = []
-    log_pattern = re.compile(r'(?P<pid>\d+)\s+(?P<time>\d+:\d+:\d+\.\d+)\s+(?P<syscall>\w+)\((?P<args>.*)\)\s+=\s(?P<retval>.+)\n')
-    unfinished_pattern = re.compile(r'(?P<pid>\d+)\s+\d+:\d+:\d+\.\d+\s+(?P<unfinished>(?P<syscall>\w+)\(.*)<unfinished\s...>\n')
-    resumed_pattern = re.compile(r'(?P<pid>\d+)\s+(?P<time>\d+:\d+:\d+\.\d+)\s+<\.\.\.\s(?P<syscall>\w+)\sresumed>(?P<resumed>.*)\n')
-    # exited_pattern = re.compile(r'(?P<pid>\d+)\s+(?P<time>\d+:\d+:\d+\.\d+)\s+\+\+\+ exited with 0 \+\+\+')
+    key = "processes"
 
-    unfinished_logs = [x for x in unfinished_pattern.finditer(strace_logs)]
-    resumed_logs = [x for x in resumed_pattern.finditer(strace_logs)]
-    concat_logs = log_concat(unfinished_logs, resumed_logs)
+    def __init__(self, logs_path):
+        """@param  logs_path: logs path."""
+        self._logs_path = logs_path
+        self.syscall_args = {}
+        self.load_syscalls_args()
 
-    normal_logs = [x for x in log_pattern.finditer(strace_logs)]
-    normal_logs.extend([x for x in log_pattern.finditer(concat_logs)])
+    def load_syscalls_args(self):
+        syscalls_json = open("/opt/CAPEv2/data/linux/linux-syscalls.json", "r")
+        syscalls_dict = json.load(syscalls_json)
+        self.syscall_args = {syscall["name"]: {"signature": syscall["signature"], "file": syscall["file"]} for syscall in syscalls_dict["syscalls"]}
 
-    for event in normal_logs:
-        pid = event.group("pid")
-        time = event.group("time")
-        syscall = event.group("syscall")
-        args = event.group("args")
-        retval = event.group("retval")
-        results.append({
-            "pid":pid,
-            "time": time,
-            "syscall": syscall,
-            "args": args,
-            "retval": retval
+    def log_concat(self, unfinished, resumed):
+        """
+        Concatenates all the respective unfinished and resumed strace logs into a string,
+        matching '<unfinished ...>' and '<... {syscall} resumed>' strings accordingly,
+        returns the `resumed` time as that is the completed syscall time.
+        """
+        data = ""
+        for head in unfinished:
+            for tail in resumed:
+                if head.group("pid") != tail.group("pid") or head.group("syscall") != tail.group("syscall"):
+                    continue
+                data += tail.group("pid") + ' ' + tail.group("time") + ' ' + head.group("unfinished") + tail.group("resumed") + '\n'
+                resumed.remove(tail)
+                unfinished.remove(head)
+                break
+        return data
+    
+    def run(self):
+        results = []
+        log_pattern = re.compile(r'(?P<pid>\d+)\s+(?P<time>\d+:\d+:\d+\.\d+)\s+(?P<syscall>\w+)\((?P<args>.*)\)\s+=\s(?P<retval>.+)\n')
+        unfinished_pattern = re.compile(r'(?P<pid>\d+)\s+\d+:\d+:\d+\.\d+\s+(?P<unfinished>(?P<syscall>\w+)\(.*)<unfinished\s...>\n')
+        resumed_pattern = re.compile(r'(?P<pid>\d+)\s+(?P<time>\d+:\d+:\d+\.\d+)\s+<\.\.\.\s(?P<syscall>\w+)\sresumed>(?P<resumed>.*)\n')
+        # exited_pattern = re.compile(r'(?P<pid>\d+)\s+(?P<time>\d+:\d+:\d+\.\d+)\s+\+\+\+ exited with 0 \+\+\+')
+        arguments_pattern = re.compile(r',\s*(?![^{}]*\})')
+
+        unfinished_logs = [x for x in unfinished_pattern.finditer(self._logs_path)]
+        resumed_logs = [x for x in resumed_pattern.finditer(self._logs_path)]
+        concat_logs = self.log_concat(unfinished_logs, resumed_logs)
+
+        normal_logs = [x for x in log_pattern.finditer(self._logs_path)]
+        normal_logs.extend([x for x in log_pattern.finditer(concat_logs)])
+
+        for event in normal_logs:
+            pid = event.group("pid")
+            time = event.group("time")
+            syscall = event.group("syscall")
+            arguments = []
+            args = arguments_pattern.split(event.group("args"))
+
+            if self.syscall_args.get(syscall, None):
+                arg_names = self.syscall_args.get(syscall).get("signature", None)
+                for arg_name, arg in zip(arg_names, args):
+                    arguments.append({
+                        "name": arg_name,
+                        "value": arg,
                     })
-    return results
+            else:
+                arguments.append(event.group("args"))
+            retval = event.group("retval")
+            results.append({
+                "pid":pid,
+                "time": time,
+                "syscall": syscall,
+                "arguments": arguments,
+                "retval": retval
+                        })
+        return results
 
 class ProcessTree():
     """ Generates process tree. """
@@ -125,7 +154,7 @@ class StraceAnalysis(Processing):
 
         strace_logs = open(strace_data_path, "r").read()
 
-        strace_behavior["processes"] = parse_log(strace_logs)
+        strace_behavior["processes"] = Processes(strace_logs).run()
         strace_behavior["processtree"] = ProcessTree(strace_data_path).run()
 
         return strace_behavior

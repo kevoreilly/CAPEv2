236cb3f7f017512d0aa3e3510cdfe7a2c73ebe66
doomedraven <doommedraven@gmail.com>
2023-12-04 09:49:54 +0100

    Update test_carbanak.py

--- tests_parsers/test_carbanak.py
+++ tests_parsers/test_carbanak.py
@@ -1,7 +1,7 @@
-from modules.processing.parsers.CAPE.SmokeLoader import extract_config
+from modules.processing.parsers.CAPE.Carbanak import extract_config
 
 
-def test_smokeloader():
+def test_carbanak():
     with open("tests/data/malware/9c9f7174d1c79569ac3464aa9a997d09d44c2094ce1b80a5e63c115edc140c56", "rb") as data:
         conf = extract_config(data.read())
         assert conf == {"c2_domains": ["blizko.net", "blizko.org"], "version": "1.7"}

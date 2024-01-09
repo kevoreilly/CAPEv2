c51ea3da6f98d0c641475e5cc209eb5372ffa780
Federico Fantini <f.fantini@certego.net>
2024-01-09 11:53:14 +0100

    fixed migration with sql

--- web/users/migrations/0003_rename_field_subscription.py
+++ web/users/migrations/0003_rename_field_subscription.py
@@ -1,4 +1,26 @@
-from django.db import migrations
+from django.db import connection, migrations
+
+
+def migrate(apps, schema_editor):
+    with schema_editor.connection.cursor() as cursor:
+        if connection.vendor == "postgresql":
+            cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'users_userprofile';")
+        elif connection.vendor == "sqlite":
+            cursor.execute("SELECT sql FROM sqlite_master WHERE tbl_name = 'users_userprofile' AND type = 'table';")
+        columns = [el[0] for el in cursor.fetchall()]
+        if "suscription" in columns:
+            cursor.execute("ALTER TABLE users_userprofile RENAME COLUMN suscription TO subscription;")
+
+
+def reverse_migrate(apps, schema_editor):
+    with schema_editor.connection.cursor() as cursor:
+        if connection.vendor == "postgresql":
+            cursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'users_userprofile';")
+        elif connection.vendor == "sqlite":
+            cursor.execute("SELECT sql FROM sqlite_master WHERE tbl_name = 'users_userprofile' AND type = 'table';")
+        columns = [el[0] for el in cursor.fetchall()]
+        if "subscription" in columns:
+            cursor.execute("ALTER TABLE users_userprofile RENAME COLUMN subscription TO suscription;")
 
 
 class Migration(migrations.Migration):
@@ -7,10 +29,4 @@ class Migration(migrations.Migration):
         ("users", "0002_reports"),
     ]
 
-    operations = [
-        migrations.RenameField(
-            model_name="userprofile",
-            old_name="suscription",
-            new_name="subscription",
-        ),
-    ]
+    operations = [migrations.RunPython(migrate, reverse_migrate)]

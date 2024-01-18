a052be52079f07bfdfd3ee46ecad14daf9c5312b
cccs-mog <117194682+cccs-mog@users.noreply.github.com>
2024-01-18 16:17:00 +0000

    Adding needed tests

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -6,10 +6,63 @@ from lib.cuckoo.common.abstracts import Signature
 from lib.cuckoo.core.plugins import RunSignatures
 from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.core.database import Database
-from lib.cuckoo.core.plugins import register_plugin
+from lib.cuckoo.core.plugins import register_plugin, list_plugins
 
-class FakeSignature(Signature):
-    name = "Fake"
+#filter_analysistypes = set(["file"])
+
+class FakeSignatureNonFiltered(Signature):
+    name = "FakeSig"
+    description = "Fake signature created for testing signatures triggering"
+    severity = 1
+    categories = ["malware"]
+    authors = ["cccs-mog"]
+    minimum = "1.3"
+    evented = True
+
+    filter_apinames = set(["gethostbyname"])
+    filter_processnames = set(["powershell.exe"])
+    filter_categories = set(["network"])    
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.query = False
+
+    def on_call(self, call, process):
+        self.query = True
+            
+
+    def on_complete(self):
+        if self.query:
+            return True
+
+class FakeSignatureFiltered(Signature):
+    name = "FakeSigFiltered"
+    description = "Fake signature created for testing signatures triggering"
+    severity = 1
+    categories = ["malware"]
+    authors = ["cccs-mog"]
+    minimum = "1.3"
+    evented = True
+
+    filter_apinames = set(["gethostbyname"])
+    filter_processnames = set(["powershell.exe"])
+    filter_categories = set(["network"])    
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.query = False
+
+    def on_call(self, call, process):
+        if call["api"] == "gethostbyname" and call["category"] == "network" and process["process_name"] == "powershell.exe":
+            self.query = True
+            
+
+    def on_complete(self):
+        if self.query:
+            return True
+
+class FakeSignatureAPI(Signature):
+    name = "FakeAPI"
     description = "Fake signature created for testing signatures triggering"
     severity = 1
     categories = ["malware"]
@@ -31,12 +84,63 @@ class FakeSignature(Signature):
     def on_complete(self):
         if self.query_host:
             return True
+        
+class FakeSignatureProcess(Signature):
+    name = "FakeProcess"
+    description = "Fake signature created for testing signatures triggering"
+    severity = 1
+    categories = ["malware"]
+    authors = ["cccs-mog"]
+    minimum = "1.3"
+    evented = True
+
+    filter_processnames = set(["powershell.exe"])
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.query_process = False
+
+    def on_call(self, call, process):
+        if process["process_name"] == "powershell.exe":
+            self.query_process = True
+            
+
+    def on_complete(self):
+        if self.query_process:
+            return True
+        
+class FakeSignatureCategory(Signature):
+    name = "FakeCategory"
+    description = "Fake signature created for testing signatures triggering"
+    severity = 1
+    categories = ["malware"]
+    authors = ["cccs-mog"]
+    minimum = "1.3"
+    evented = True
+
+    filter_categories = set(["network"])
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.query_network = False
+
+    def on_call(self, call, process):
+        if call["category"] == "network":
+            self.query_network = True
+            
+
+    def on_complete(self):
+        if self.query_network:
+            return True
 
 class TestSignatureEngine:
     def setup_method(self, method):
         self.d = Database(dsn="sqlite://")
-        register_plugin("signatures", FakeSignature)
-        
+        register_plugin("signatures", FakeSignatureAPI)
+        register_plugin("signatures", FakeSignatureProcess)
+        register_plugin("signatures", FakeSignatureCategory)
+        register_plugin("signatures", FakeSignatureNonFiltered)
+        register_plugin("signatures", FakeSignatureFiltered)
     @pytest.mark.parametrize(
         "task_id, signature_name, match_expected",
         # @task_id: task to be created or task id to use
@@ -49,16 +153,64 @@ class TestSignatureEngine:
                 "",
                 False,
             ),
-            # Single signature which should match
+            # Single signature with API filtering which should match
             (
                 1,
-                "Fake",
+                "FakeAPI",
                 True,
             ),
-            # Single signature which shouldn't match
+            # Single signature with API filtering which shouldn't match
             (
                 2,
-                "Fake",
+                "FakeAPI",
+                False,
+            ),
+            # Single signature with category filtering  which should match
+            (
+                2,
+                "FakeCategory",
+                True,
+            ),
+            # Single signature with category filtering  which shouldn't match
+            (
+                3,
+                "FakeCategory",
+                False,
+            ),
+            # Single signature with process filtering  which should match
+            (
+                2,
+                "FakeProcess",
+                True,
+            ),
+            # Single signature with process filtering  which shouldn't match
+            (
+                3,
+                "FakeProcess",
+                False,
+            ),
+            # Single signature with all filtering  which should match
+            (
+                1,
+                "FakeSig",
+                True,
+            ),
+            # Single signature with all filtering  which should match
+            (
+                2,
+                "FakeSig",
+                False,
+            ),
+            # Single signature with all double filtering which should match
+            (
+                1,
+                "FakeSigFiltered",
+                True,
+            ),
+            # Single signature with all double filtering which shouldn't match
+            (
+                2,
+                "FakeSigFiltered",
                 False,
             ),
             # Test running all signatures
@@ -76,7 +228,7 @@ class TestSignatureEngine:
         report = None
         results = {}
         if task_id is not None:
-            report = os.path.join(CUCKOO_ROOT, "tests", "test_data", str(task_id), "reports", "report.json")
+            report = os.path.join(CUCKOO_ROOT, "tests", "data", str(task_id), "reports", "report.json")
             assert path_exists(report),"Missing test data file, failing"
         if report:
             results = json.load(open(report))

cd9ad210c3deb2275f0a8a5b879ce2ce0c2e300e
cccs-mog <117194682+cccs-mog@users.noreply.github.com>
2024-01-18 11:54:47 -0500

    Update tests/test_signature.py
    Co-authored-by: Kevin <59843993+cccs-kevin@users.noreply.github.com>

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -8,7 +8,6 @@ from lib.cuckoo.common.constants import CUCKOO_ROOT
 from lib.cuckoo.core.database import Database
 from lib.cuckoo.core.plugins import register_plugin
 
-#filter_analysistypes = set(["file"])
 
 class FakeSignatureNonFiltered(Signature):
     name = "FakeSig"

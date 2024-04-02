48d59c9050b69714ae178d55b3bf74283bb68744
doomedraven <doommedraven@gmail.com>
2024-04-02 15:14:47 +0200

    Update test_cleaners_utils.py

--- tests/test_cleaners_utils.py
+++ tests/test_cleaners_utils.py
@@ -2,9 +2,9 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
-from lib.cuckoo.common import cleaner_utils
+from lib.cuckoo.common import cleaners_utils
 
 
 def test_free_space_monitor(mocker):
     # Will not enter main loop
-    cleaner_utils.free_space_monitor(return_value=True)
+    cleaners_utils.free_space_monitor(return_value=True)

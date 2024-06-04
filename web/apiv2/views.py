382c9655efd008908f86f63a3c197d9a942794db
doomedraven <doommedraven@gmail.com>
2024-06-04 08:19:00 +0200

    Update views.py

--- web/apiv2/views.py
+++ web/apiv2/views.py
@@ -1097,8 +1097,7 @@ def tasks_status(request, task_id):
         status = task.to_dict()["status"]
         resp = {"error": False, "data": status}
     elif request.method == "POST" and apiconf.user_stop.enabled and request.data.get("status", "") == "finish":
-        machine_name = task.guest.name
-        machine = db.view_machine(machine_name)
+        machine = db.view_machine(task.guest.name)
         # Todo probably add task status if pending
         if machine.status == "running":
             try:

2bce45de52b39c88c2aab30f31e384508b6171b8
Karl Hiramoto <karlh@google.com>
2024-06-10 13:19:06 +0000

    on linux  join() must be called to release socket

--- agent/test_agent.py
+++ agent/test_agent.py
@@ -206,7 +206,6 @@ class TestAgent:
             r = requests.get(f"{BASE_URL}/kill")
             assert r.status_code == 200
             assert r.json()["message"] == "Quit the CAPE Agent"
-            self.agent_process.terminate()
         except requests.exceptions.ConnectionError:
             pass
         shutil.rmtree(DIRPATH, ignore_errors=True)

e6518e2d76bacd4d2b40cc004285f8531b2459b1
Takashi Matsumoto <77025706+t-mtsmt@users.noreply.github.com>
2024-07-07 02:54:48 +0900

    Add error handling

--- modules/machinery/vmwarerest.py
+++ modules/machinery/vmwarerest.py
@@ -49,12 +49,72 @@ class VMwareREST(Machinery):
 
         log.info("VMwareREST machinery module initialised (%s:%s)", self.host, self.port)
 
+    def check_response(self, response):
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 400:
+            raise CuckooMachineError("VMwareREST: Invalid parameters")
+        elif response.status_code == 401:
+            raise CuckooMachineError("VMwareREST: Authentication failed, please check credentials in vmwarerest.conf")
+        elif response.status_code == 403:
+            raise CuckooMachineError("VMwareREST: Permission denied")
+        elif response.status_code == 404:
+            raise CuckooMachineError("VMwareREST: No such resource")
+        elif response.status_code == 406:
+            raise CuckooMachineError("VMwareREST: Content type was not supported")
+        elif response.status_code == 409:
+            raise CuckooMachineError("VMwareREST: Resource state conflicts")
+        elif response.status_code == 500:
+            raise CuckooMachineError("VMwareREST: Server error")
+        else:
+            raise CuckooMachineError("VMwareREST: Unexpected error")
+
     def get_vms(self):
-        vms = s.get(f"{self.api_url}/vms", auth=(self.username, self.password))
-        if "Authentication failed" in vms.text:
-            log.info("Authentication failed, please check credentials in vmwarerest.conf")
-            return None
-        return vms.json()
+        """Returns a list of VM IDs and paths for all VMs."""
+        try:
+            response = s.get(
+                f"{self.api_url}/vms",
+                auth=(self.username, self.password),
+                headers={
+                    "Accept": "application/vnd.vmware.vmw.rest-v1+json"
+                },
+            )
+        except Exception:
+            raise CuckooMachineError(f"VMwareREST: Couldn't connect to vmrest server.")
+
+        return self.check_response(response)
+
+    def get_power(self, vmmoid):
+        """Returns the power state of the VM."""
+        try:
+            response = s.get(
+                f"{self.api_url}/vms/{vmmoid}/power",
+                auth=(self.username, self.password),
+                headers={
+                    "Accept": "application/vnd.vmware.vmw.rest-v1+json"
+                },
+            )
+        except Exception:
+            raise CuckooMachineError(f"VMwareREST: Couldn't connect to vmrest server.")
+
+        return self.check_response(response)
+
+    def change_power_state(self, vmmoid, operation):
+        """Changes the VM power state."""
+        try:
+            response = s.put(
+                f"{self.api_url}/vms/{vmmoid}/power",
+                auth=(self.username, self.password),
+                data=operation,
+                headers={
+                    "Content-Type": "application/vnd.vmware.vmw.rest-v1+json",
+                    "Accept": "application/vnd.vmware.vmw.rest-v1+json",
+                },
+            )
+        except Exception:
+            raise CuckooMachineError(f"VMwareREST: Couldn't connect to vmrest server.")
+
+        return self.check_response(response)
 
     def get_vmmoid(self, id):
         vms = self.get_vms()
@@ -62,45 +122,27 @@ class VMwareREST(Machinery):
             for vm in vms:
                 if vm["path"].endswith(f"{id}.vmx"):
                     return vm["id"]
-        log.info("There was a problem getting vmmoid for vm %s", id)
+        raise CuckooMachineError("There was a problem getting vmmoid for vm %s", id)
 
     def poweron_vm(self, id):
         vmmoid = self.get_vmmoid(id)
         if vmmoid:
             log.info("Powering on vm %s", id)
-            status = s.put(
-                f"{self.api_url}/vms/{vmmoid}/power",
-                auth=(self.username, self.password),
-                data="on",
-                headers={"content-type": "application/vnd.vmware.vmw.rest-v1+json"},
-            )
-            if "Authentication failed" in status.text:
-                log.info("Authentication failed, please check credentials in vmwarerest.conf")
-                return None
-            return status
-        log.info("There was a problem powering on vm %s", id)
+            return self.change_power_state(vmmoid, "on")
+        raise CuckooMachineError("There was a problem powering on vm %s", id)
 
     def poweroff_vm(self, id):
         vmmoid = self.get_vmmoid(id)
         if vmmoid:
             log.info("Powering off vm %s", id)
-            return s.put(
-                f"{self.api_url}/vms/{vmmoid}/power",
-                auth=(self.username, self.password),
-                data="off",
-                headers={"content-type": "application/vnd.vmware.vmw.rest-v1+json"},
-            )
-        log.info("There was a problem powering off vm %s", id)
+            return self.change_power_state(vmmoid, "off")
+        raise CuckooMachineError("There was a problem powering off vm %s", id)
 
     def get_power_for_vm(self, id):
         vmmoid = self.get_vmmoid(id)
         if vmmoid:
-            return s.get(
-                f"{self.api_url}/vms/{vmmoid}/power",
-                auth=(self.username, self.password),
-            )
-
-        log.info("There was a problem querying power status for vm %s", id)
+            return self.get_power(vmmoid)
+        raise CuckooMachineError("There was a problem querying power status for vm %s", id)
 
     def start(self, id):
         log.info("Starting vm %s", id)
@@ -116,8 +158,9 @@ class VMwareREST(Machinery):
         log.info("Checking vm %s", id)
         power_state = self.get_power_for_vm(id)
 
-        if power_state and "poweredOn" in power_state.text:
+        if power_state["power_state"] == "poweredOn":
             log.info("Vm %s is running", id)
-            return id
+            return True
         else:
             log.info("Vm %s is not running", id)
+            return False

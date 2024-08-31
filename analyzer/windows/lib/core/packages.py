217fe3c86ebe7b8d30ad3a42dc60be1281ed2403
Mohannad Raafat <62453654+para0x0dise@users.noreply.github.com>
2024-08-31 14:32:12 +0300

    Update packages.py

--- analyzer/windows/lib/core/packages.py
+++ analyzer/windows/lib/core/packages.py
@@ -69,7 +69,7 @@ def choose_package(file_type, file_name, exports, target):
         return "rar"
     elif "Macromedia Flash" in file_type or file_name.endswith((".swf", ".fws")):
         return "swf"
-    elif file_name.endswith((".py", ".pyc")) or "Python script" in file_type:
+    elif file_name.endswith((".py", ".pyc")) or "Python script" in file_type or b"import" in file_content:
         return "python"
     elif file_name.endswith(".ps1"):
         return "ps1"

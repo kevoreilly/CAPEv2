b960ae6748a1fee3f81f4ecac91a574b7aac29eb
Yung Binary <93540406+YungBinary@users.noreply.github.com>
2024-10-23 20:43:10 +0000

    Add lumma

--- tests_parsers/test_lumma.py
+++ tests_parsers/test_lumma.py
@@ -9,3 +9,4 @@ def test_lumma():
     with open("tests/data/malware/5d58bc449693815f6fb0755a364c4cd3a8e2a81188e431d4801f2fb0b1c2de8f", "rb") as data:
         conf = extract_config(data.read())
         assert conf == {'C2': ['delaylacedmn.site', 'writekdmsnu.site', 'agentyanlark.site', 'bellykmrebk.site', 'underlinemdsj.site', 'commandejorsk.site', 'possiwreeste.site', 'famikyjdiag.site', 'agentyanlark.site']}
+

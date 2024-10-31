23869a168caf2c85e8282a3215946327fec1334f
Yung Binary <93540406+YungBinary@users.noreply.github.com>
2024-10-31 22:49:14 +0000

    Update stealc test

--- tests_parsers/test_stealc.py
+++ tests_parsers/test_stealc.py
@@ -9,5 +9,5 @@ def test_stealc():
     with open("tests/data/malware/619751f5ed0a9716318092998f2e4561f27f7f429fe6103406ecf16e33837470", "rb") as data:
         conf = extract_config(data.read())
         assert conf == {
-            "C2": ["http://95.217.125.57"],
+            "C2": ["http://95.217.125.57/2f571d994666c8cb.php"],
         }

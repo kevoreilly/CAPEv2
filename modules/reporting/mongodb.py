kevoreilly/CAPEv2

commit 5329b7a85485e9aa0cc57acc3d5349904f6e961d

fix MongoDB BSONObjectTooLarge conditions

modules/reporting/mongodb.py
@@ -131,9 +131,10 @@ class MongoDB(Report):
         try:
             mongo_insert_one("analysis", report)
         except OperationFailure as e:
-            # ToDo rewrite how children are stored
-            if str(e).startswith("BSONObj exceeds maximum nested object"):
-                log.debug("Deleting behavior process tree children from results.")
+            # check for error code 10334, BSONObjectTooLarge
+            if e.code == 10334:
+                # ToDo rewrite how children are stored
+                log.warning("Deleting behavior process tree children from results.")
                 del report["behavior"]["processtree"][0]["children"]
                 try:
                     mongo_insert_one("analysis", report)
@@ -141,6 +142,8 @@ class MongoDB(Report):
                     log.error("Deleting behavior process tree parent from results: %s", str(e))
                     del report["behavior"]["processtree"][0]
                     mongo_insert_one("analysis", report)
+            else:
+                raise CuckooReportError("Failed inserting report in Mongo") from e
         except InvalidDocument as e:
             if str(e).startswith("cannot encode object") or "must not contain" in str(e):
                 self.loop_saver(report)

commit 521bdb1ee124b8616ce878a52f6d93f9b1c4f0f0

check for MongoDB code 15 Overflow too
This extends the error code checks introduced in 5329b7a.

modules/reporting/mongodb.py
@@ -131,8 +131,11 @@ class MongoDB(Report):
         try:
             mongo_insert_one("analysis", report)
         except OperationFailure as e:
-            # check for error code 10334, BSONObjectTooLarge
-            if e.code == 10334:
+            # Check for error codes indicating the BSON object was too large
+            # (10334 BSONObjectTooLarge) or the maximum nested object depth was
+            # exceeded (15 Overflow).
+            if e.code in (10334, 15):
+                log.error("Got MongoDB OperationFailure, code %d", e.code)
                 # ToDo rewrite how children are stored
                 log.warning("Deleting behavior process tree children from results.")
                 del report["behavior"]["processtree"][0]["children"]
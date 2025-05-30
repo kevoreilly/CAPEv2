852b86fd2e77bfec68d261f4b6739461e16d4b4e
doomedraven <abrukhovetskyy@google.com>
2025-05-30 10:06:55 +0200

    Update startup.py

--- lib/cuckoo/core/startup.py
+++ lib/cuckoo/core/startup.py
@@ -119,7 +119,7 @@ def check_webgui_mongo():
         # Note: Silently ignores the creation if the index already exists.
         mongo_create_index("analysis", "info.id", name="info.id_1")
         # Some indexes that can be useful for some users
-        mongo_create_index("files", "md5", name="target_md5")
+        mongo_create_index("files", "md5", name="file_md5")
         mongo_create_index("files", [("_task_ids", 1)])
 
         # side indexes as ideas

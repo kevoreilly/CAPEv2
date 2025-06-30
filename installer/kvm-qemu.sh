674bcd7437f8c47069a92cd93169ad9808f010ec
MohammadHossein <51611794+MHM98@users.noreply.github.com>
2025-06-30 11:00:28 +0330

    Modify kvm-qemu.sh to apply changes in  #1634 back (#2606)
    Modify kvm installer to address issue #2588

--- installer/kvm-qemu.sh
+++ installer/kvm-qemu.sh
@@ -778,7 +778,7 @@ function replace_seabios_clues_public() {
 
     FILES=(
         src/hw/blockcmd.c
-        src/fw/paravirt.c
+        #src/fw/paravirt.c
     )
     for file in "${FILES[@]}"; do
         _sed_aux 's/"QEMU"/"'"$BOCHS_BLOCK_REPLACER2"'"/g' "$file" "QEMU was not replaced in $file"

8de4cb7cd0c629d1c500bf297240f6e5ac372565
doomedraven <doommedraven@gmail.com>
2025-07-14 11:12:59 +0200

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -768,7 +768,7 @@ class TestDatabaseEngine:
 
         with db.session.begin():
             exp_val = dict(**dct, parent=None, id=sample_id)
-            assert db.get_parent_sample_from_task(task_id=task_id) == exp_val
+            assert db.get_parent_sample_from_task(task_id=task_id).to_dict() == exp_val
             assert db.get_parent_sample_from_task(task_id=task_id + 1) == {}
 
     def test_list_tasks(self, db: _Database, temp_filename, freezer):

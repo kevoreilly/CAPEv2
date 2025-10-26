fc8b3fdfecbcb346ff494d2f513527d2a8882a1e
Kev <km9530802@gmail.com>
2025-10-26 20:10:59 +0100

    feat: resolve comments

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,17 @@
-source venv/bin/activate
+#!/bin/bash
+set -e
+
+cd /cape
+
+cd web
+python manage.py migrate
+cd ..
+
 python cuckoo.py &
+CUCKOO_PID=$!
 
 cd web
 
 : "${WEB_PORT:=8000}"
 
-python manage.py migrate
 python manage.py runserver 0.0.0.0:${WEB_PORT}

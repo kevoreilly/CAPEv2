3388985c7248df34829f06a15567bab26d36e9b2
enzok <7831008+enzok@users.noreply.github.com>
2025-12-18 08:02:27 -0500

    Fix search

--- lib/cuckoo/common/web_utils.py
+++ lib/cuckoo/common/web_utils.py
@@ -1430,16 +1430,17 @@ def perform_search(
             print(f"Unknown search {term}:{value}")
             return []
 
-        # Allow to overwrite perform_search_filters for custom results
-        if not projection:
-            projection = perform_search_filters
-        if "target.file.sha256" in projection:
-            projection = dict(**projection)
-            projection[f"target.file.{FILE_REF_KEY}"] = 1
-        if term in search_term_map_repetetive_blocks:
-            mongo_search_query = {"$or": [{path: condition} for path, condition in mongo_search_query.items()]}
         if not retval and mongo_search_query:
+            # Allow to overwrite perform_search_filters for custom results
+            if not projection:
+                projection = perform_search_filters
+            if "target.file.sha256" in projection:
+                projection = dict(**projection)
+                projection[f"target.file.{FILE_REF_KEY}"] = 1
+            if term in search_term_map_repetetive_blocks:
+                mongo_search_query = {"$or": [{path: condition} for path, condition in mongo_search_query.items()]}
             retval = list(mongo_find("analysis", mongo_search_query, projection, limit=search_limit))
+
         for doc in retval:
             target_file = doc.get("target", {}).get("file", {})
             if FILE_REF_KEY in target_file and "sha256" not in target_file:

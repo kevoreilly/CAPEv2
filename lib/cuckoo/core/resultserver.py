2f1177340187125c0b5b7440a6f58f0fa473023c
doomedraven <abrukhovetskyy@google.com>
2025-12-30 12:59:31 +0000

    close_fds

--- lib/cuckoo/core/resultserver.py
+++ lib/cuckoo/core/resultserver.py
@@ -479,10 +479,7 @@ class GeventResultServerWorker(gevent.server.StreamServer):
             for ctx in ctxs:
                 log.debug("Task #%s: Cancel %s", task_id, ctx)
                 ctx.cancel()
-            # ToDo just reinforce cleanup
             task_log_stop_force(task_id)
-
-        task_log_stop_force(task_id)
 
     def create_folders(self):
         for folder in list(RESULT_UPLOADABLE) + [b"logs"]:

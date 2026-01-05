090ea2b10f501c058690f82f0fd1f2c53190fd74
Josh Feather <142008135+josh-feather@users.noreply.github.com>
2026-01-05 17:49:22 +0000

    Fix Linux analyzer test case

--- analyzer/linux/tests/modules/packages/test_package.py
+++ analyzer/linux/tests/modules/packages/test_package.py
@@ -31,5 +31,5 @@ class TestPackage(unittest.TestCase):
 
     @pytest.mark.usefixtures("patch_netlogfile")
     def test_package_init_args_str(self):
-        pkg = Package(sys.executable, options={"arguments": "foo, bar"})
+        pkg = Package(sys.executable, options={"arguments": "foo bar"})
         self.assertEqual(pkg.args, ["foo", "bar"])

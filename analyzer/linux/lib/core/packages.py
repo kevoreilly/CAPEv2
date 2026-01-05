kevoreilly/CAPEv2

commit d9be04306ebab01a031f7424a64171cfb9b7a99d

pass always args as list for commandline compatibility (#2800)

analyzer/linux/lib/core/packages.py
@@ -101,7 +101,9 @@ class Package:
         self.timeout = kwargs.get("timeout")
         # Command-line arguments for the target.
 
-        self.args = self.options.get("arguments", [])
+        _args = self.options.get("arguments", [])
+        if isinstance(_args, str):
+            self.args = _args.split()
         # Choose an analysis method (or fallback to apicalls)
         self.method = self.options.get("method", "apicalls")
         # Should our target be launched as root or not
@@ -202,11 +204,7 @@ class Package:
 
         target_cmd = f"{self.target}"
         if "args" in kwargs:
-            args = kwargs["args"]
-            if not isinstance(args, str):
-                args = " ".join(args)
-            target_cmd += f" {args}"
-
+            target_cmd += f' {" ".join(kwargs["args"])}'
 
         # eg: strace_args=-e trace=!recvfrom;epoll_pwait
         strace_args = self.options.get("strace_args", "").replace(";", ",")

commit 7f780cc9255a5ca0f587de9f5d724d2c50a80903

Bugfix: Always initialize Package.args in Linux Analyzer Package
Fixes a bug introduced in d9be043 that prevents Package.args from being set if a string was not passed into the object during initialization. Package.args will now always be initialized with an empty list if no arguments are provided. This behavior aligns with the Windows analyzer.

analyzer/linux/lib/core/packages.py
@@ -101,9 +101,17 @@ class Package:
         self.timeout = kwargs.get("timeout")
         # Command-line arguments for the target.
 
-        _args = self.options.get("arguments", [])
-        if isinstance(_args, str):
-            self.args = _args.split()
+        def _args():
+            retval = []
+            args = self.options.get("arguments")
+            if isinstance(args, list):
+                retval = args
+            elif isinstance(args, str):
+                retval = args.split()
+            return retval
+
+        self.args = _args()
+
         # Choose an analysis method (or fallback to apicalls)
         self.method = self.options.get("method", "apicalls")
         # Should our target be launched as root or not
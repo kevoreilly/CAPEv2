463f35e8164543217b5b2710d1d66726d54545b8
doomedraven <doommedraven@gmail.com>
2026-05-31 22:46:48 +0200

    Add env config support for cape-processor
    Add support for .env configuration for the CAPE processor and wire those variables into the service and runtime. Changes include: copying conf/default/*.env in installer and copy script; adding a default conf/default/cape-processor.env; updating systemd service to load environment variables and files, set sensible defaults and use utils/process.py as ExecStart; and updating utils/process.py to read configuration from environment variables (with a str_to_bool helper) for ID, parallelism, timeouts, memory flags, and other options. Also adjusted service RestartSec. This enables easier configuration via environment files or systemd Environment settings.

--- conf/copy_configs.sh
+++ conf/copy_configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for filename in conf/default/*.conf.default; do
+for filename in conf/default/*.conf.default conf/default/*.env; do
     cp -vf "./$filename" "./$(echo "$filename" | sed -e 's/.default//g' | sed -e 's/default//g')";
 done

11e088f07fc716e307ebde1b516ec209e288b4fb
doomedraven <abrukhovetskyy@google.com>
2026-08-23 08:58:29 +0000

    Update SCHEMA_VERSION to errors_task_id_cascade

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -49,7 +49,7 @@ except ImportError:  # pragma: no cover
 
 
 
-SCHEMA_VERSION = "3a1b_tenant_visibility"
+SCHEMA_VERSION = "errors_task_id_cascade"
 
 log = logging.getLogger(__name__)
 conf = Config("cuckoo")

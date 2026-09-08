aff4d6f2592a391f7aea5f4c5ece87000c715f5f
doomedraven <doommedraven@gmail.com>
2026-09-08 11:49:43 +0200

    python

--- analyzer/windows/modules/packages/python.py
+++ analyzer/windows/modules/packages/python.py
@@ -2,6 +2,8 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
+import os
+
 from lib.common.abstracts import Package
 from lib.common.common import check_file_extension
 from lib.common.constants import OPT_ARGUMENTS
@@ -23,6 +25,13 @@ class Python(Package):
             python = self.get_path_glob("python.exe")
         except CuckooPackageError:
             python = self.get_path_glob("py.exe")
+
+        # Set PYTHONHOME to help Python locate its standard library during initialization.
+        # This is critical when the process is monitored via DLL injection, as the monitoring
+        # can interfere with Python's module search path. Without this, Python may fail to
+        # load the 'encodings' module, resulting in: "ModuleNotFoundError: No module named 'encodings'"
+        python_home = os.path.dirname(python)
+        os.environ["PYTHONHOME"] = python_home
 
         arguments = self.options.get(OPT_ARGUMENTS, "")
 
